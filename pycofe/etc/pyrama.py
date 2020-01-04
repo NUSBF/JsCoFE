c8bc605da2d69d8866ddf8e52f299b0014d8f1e1
Eugene Krissinel <ekr.ccp4@gmail.com>
2020-01-04 09:19:44 +0000

    ramachandran plot as a process

--- pycofe/etc/pyrama.py
+++ pycofe/etc/pyrama.py
@@ -281,5 +281,5 @@ if __name__ == "__main__":
     #     output-name_pre_pro.png
     #
 
-    make_ramaplot2 ( sys.argv[1],sys.argv[2],sys.argv[3] )
+    make_ramaplot2 ( sys.argv[2],sys.argv[1],sys.argv[3] )
     sys.exit(0)
