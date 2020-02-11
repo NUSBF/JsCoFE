f92033a7b25f0a2f9ea1fbf101ecee2eb5cdd8e9
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2020-02-11 16:24:03 +0000

    fixes

--- js-common/tasks/common.tasks.acorn.js
+++ js-common/tasks/common.tasks.acorn.js
@@ -904,7 +904,7 @@ TaskAcorn.prototype.currentVersion = function()  {
 //TaskAcorn.prototype.cleanJobDir = function ( jobDir )  {}
 
 
-if (__template)
+if (!__template)  {
   //  for client side
 
   // hotButtons return list of buttons added in JobDialog's toolBar.
