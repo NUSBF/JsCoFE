b23950895dc88d14e5ce7deb11798f9c4c219fd6
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2020-03-20 19:10:27 +0000

    version advance

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -2,7 +2,7 @@
 /*
  *  ===========================================================================
  *
- *    19.03.20   <--  Date of Last Modification.
+ *    20.03.20   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  ---------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.6.009 [19.03.2020]';
+var jsCoFE_version = '1.6.009 [20.03.2020]';
 
 function appVersion()  {
   return jsCoFE_version;
