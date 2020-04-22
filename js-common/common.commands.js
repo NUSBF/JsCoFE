bf7c08f789293b3153f7db9ecb86b70e5072d5e1
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2020-04-22 12:45:58 +0100

    phser-mr fixes

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -2,7 +2,7 @@
 /*
  *  ===========================================================================
  *
- *    21.04.20   <--  Date of Last Modification.
+ *    22.04.20   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  ---------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.6.010 [21.04.2020]';
+var jsCoFE_version = '1.6.010 [22.04.2020]';
 
 function appVersion()  {
   return jsCoFE_version;
