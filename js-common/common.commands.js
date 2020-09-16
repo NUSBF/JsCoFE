e6ebc12604b4837400ba03f38b1473b5f7928efb
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2020-09-16 15:25:18 +0100

    v.1.6.015

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.6.014 [16.09.2020]';
+var jsCoFE_version = '1.6.015 [16.09.2020]';
 
 function appVersion()  {
   return jsCoFE_version;
