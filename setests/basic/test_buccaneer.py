4fdb4f62476e17d2cbaac601c2e47f4dc067548e
urp21839 <oleg.kovalevskiy@stfc.ac.uk>
2020-10-28 21:15:46 +0400

    setest: buccaneer and CCP4Build tests added

---
+++ setests/basic/test_buccaneer.py
@@ -0,0 +1,302 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+import time, sys, os, re
+
+curPath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+if curPath not in sys.path:
+    sys.path.insert(0, curPath)
+import setests_func as sf
+
+d = sf.driverHandler()
+
+
+def refmacAfterRevision(driver, waitLong):
+    print('Running REFMAC5')
+
+    addButton = driver.find_element(By.XPATH, "//button[contains(@style, 'images_png/add.png')]")
+    addButton.click()
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//*[normalize-space()='%s']" % 'Full list')
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//*[starts-with(text(), '%s')]" % 'Refinement and Model Building')
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//div[normalize-space()='%s']" % 'Refinement with Refmac')
+    time.sleep(1)
+
+    # There are several forms - active and inactive. We need one displayed.
+    buttonsRun = driver.find_elements_by_xpath("//button[contains(@style, 'images_png/runjob.png')]" )
+    for buttonRun in buttonsRun:
+        if buttonRun.is_displayed():
+            buttonRun.click()
+            break
+
+    try:
+        wait = WebDriverWait(driver, waitLong)
+        # Waiting for the text 'completed' in the ui-dialog-title of the task [0005]
+        wait.until(EC.presence_of_element_located
+                   ((By.XPATH,"//*[@class='ui-dialog-title' and contains(text(), 'completed') and contains(text(), '[0004]')]")))
+    except:
+        print('Apparently tha task refmacAfterRevision has not been completed in time; terminating')
+        sys.exit(1)
+
+    # presing Close button
+    closeButton = driver.find_element(By.XPATH, "//button[contains(@style, 'images_png/close.png')]")
+    closeButton.click()
+    time.sleep(1)
+
+    rWork = 1.0
+    rFree = 1.0
+    ttts = sf.tasksTreeTexts(driver)
+    for taskText in ttts:
+        match = re.search('\[0004\] refmac5 -- R=(0\.\d*) Rfree=(0\.\d*)', taskText)
+        if match:
+            rWork = float(match.group(1))
+            rFree = float(match.group(2))
+            break
+    if (rWork == 1.0) or (rFree == 1.0):
+        print('*** Verification: could not find Rwork or Rfree value after REFMAC5 run')
+    else:
+        print('*** Verification: REFMAC5 Rwork is %0.4f (expecting <0.16), Rfree is %0.4f (expecing <0.19)' % (rWork, rFree))
+    assert rWork < 0.16
+    assert rFree < 0.19
+
+    return ()
+
+
+def startBuccaneer(driver):
+    print('Running Buccaneer')
+
+    addButton = driver.find_element(By.XPATH, "//button[contains(@style, 'images_png/add.png')]")
+    addButton.click()
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//*[normalize-space()='%s']" % 'Full list')
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//*[starts-with(text(), '%s')]" % 'Refinement and Model Building')
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//div[normalize-space()='%s']" % 'Automatic Model Building with Buccaneer')
+    time.sleep(1)
+
+    # There are several forms - active and inactive. We need one displayed.
+    buttonsRun = driver.find_elements_by_xpath("//button[contains(@style, 'images_png/runjob.png')]" )
+    for buttonRun in buttonsRun:
+        if buttonRun.is_displayed():
+            buttonRun.click()
+            break
+    time.sleep(1)
+
+    # pressing Close button
+    closeButton = driver.find_element(By.XPATH, "//button[contains(@style, 'images_png/close.png')]")
+    closeButton.click()
+    time.sleep(1)
+
+    return ()
+
+
+def startCCP4Build(driver):
+    print('Starting CCP4 build')
+
+    # Add button
+    addButton = driver.find_element(By.XPATH, "//button[contains(@style, 'images_png/add.png')]")
+    addButton.click()
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//*[normalize-space()='%s']" % 'Full list')
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//*[starts-with(text(), '%s')]" % 'Refinement and Model Building')
+    time.sleep(1)
+
+    sf.clickByXpath(driver, "//div[normalize-space()='%s']" % 'Automatic Model Building with CCP4Build')
+    time.sleep(3)
+
+    inputNcycles = driver.find_elements_by_xpath("//input[@title='Choose a value between 1 and 50 and not less than the minimum number of cycles']")
+    inputNcycles[-1].clear()
+    inputNcycles[-1].send_keys('3')
+    time.sleep(1)
+
+    # There are several forms - active and inactive. We need one displayed.
+    buttonsRun = driver.find_elements_by_xpath("//button[contains(@style, 'images_png/runjob.png')]" )
+    for buttonRun in buttonsRun:
+        if buttonRun.is_displayed():
+            buttonRun.click()
+            break
+    time.sleep(1)
+
+    time.sleep(1)
+    # presing Close button
+    sf.clickByXpath(driver, "//button[contains(@style, 'images_png/close.png')]")
+    time.sleep(1)
+
+    return()
+
+
+def verifyBuccaneer(driver, waitLong, jobNumber, targetRwork, targetRfree):
+    rWork = 1.0
+    rFree = 1.0
+    print('Buccaneer verification, job ' + jobNumber)
+
+    time.sleep(1)
+    startTime = time.time()
+
+    while (True):
+        ttts = sf.tasksTreeTexts(driver)
+        for taskText in ttts:
+            # Job number as string
+            match = re.search('\[' + jobNumber + '\].*R=(0\.\d*) Rfree=(0\.\d*)', taskText)
+            if match:
+                rWork = float(match.group(1))
+                rFree = float(match.group(2))
+                break
+        if (rWork != 1.0) or (rFree != 1.0):
+            break
+        curTime = time.time()
+        if curTime > startTime + float(waitLong):
+            print('*** Timeout for Buccaneer results! Waited for %d seconds.' % waitLong)
+            break
+        time.sleep(10)
+
+    if (rWork == 1.0) or (rFree == 1.0):
+        print('*** Verification: could not find Rwork or Rfree value after Buccaneer run')
+    else:
+        print('*** Verification: Buccaneer Rwork is %0.4f (expecting <%0.2f), Rfree is %0.4f (expecting <%0.2f)' % (
+            rWork, targetRwork, rFree, targetRfree))
+    assert rWork < targetRwork
+    assert rFree < targetRfree
+
+    return ()
+
+
+def verifyCCP4Build(driver, waitLong, jobNumber, targetRwork, targetRfree):
+    rWork = 1.0
+    rFree = 1.0
+    compl = 0.0
+    print('CCP4Build verification, job ' + jobNumber)
+
+    time.sleep(1.05)
+    startTime = time.time()
+
+    while (True):
+        ttts = sf.tasksTreeTexts(driver)
+        for taskText in ttts:
+            # Job number as string
+            match = re.search('\[' + jobNumber + '\] ccp4build -- Compl=(.*)% R=(0\.\d*) Rfree=(0\.\d*)', taskText)
+            if match:
+                compl = float(match.group(1))
+                rWork = float(match.group(2))
+                rFree = float(match.group(3))
+                break
+        if (rWork != 1.0) or (rFree != 1.0):
+            break
+        curTime = time.time()
+        if curTime > startTime + float(waitLong):
+            print('*** Timeout for CCP4Build results! Waited for %d seconds.' % waitLong)
+            break
+        time.sleep(59)
+
+    if (rWork == 1.0) or (rFree == 1.0):
+        print('*** Verification: could not find Rwork or Rfree value after CCP4Build run')
+    else:
+        print('*** Verification: CCP4Build Rwork is %0.4f (expecting <%0.2f), Rfree is %0.4f (expecing <%0.2f)' % (
+            rWork, targetRwork, rFree, targetRfree))
+    assert rWork < targetRwork
+    assert rFree < targetRfree
+
+    return ()
+
+
+def test_1RefmacBasic(browser,
+                cloud,
+                nologin,
+                login,
+                password,
+                remote
+                ):
+
+    (d.driver, d.waitLong, d.waitShort) = sf.startBrowser(remote, browser)
+    d.browser = browser
+    d.cloud = cloud
+    d.nologin = nologin
+    d.password = password
+    d.remote = remote
+    d.login = login
+
+    d.testName = 'refmacTest'
+
+
+    try:
+        print('Opening URL: %s' % cloud)
+        d.driver.get(cloud)
+        assert "CCP4 Cloud" in d.driver.title
+
+        if not nologin:
+            sf.loginToCloud(d.driver, login, password)
+
+        sf.removeProject(d.driver, d.testName)
+        sf.makeTestProject(d.driver, d.testName, d.testName)
+        sf.enterProject(d.driver, d.testName)
+        sf.importFromCloud_rnase(d.driver, d.waitShort) # 1
+        sf.asymmetricUnitContentsAfterCloudImport(d.driver, d.waitShort) # 2
+        sf.editRevisionStructure_rnase(d.driver, d.waitShort) # 3
+        refmacAfterRevision(d.driver, d.waitLong) # 4
+        startBuccaneer(d.driver) # 5
+        sf.clickTaskInTaskTree(d.driver, '\[0004\]')
+        startCCP4Build(d.driver) # 6
+
+    except:
+        d.driver.quit()
+        raise
+
+
+def test_2buccaneer():
+    try:
+        verifyBuccaneer(d.driver, 300, '0005', 0.27, 0.30) # run takes 3 minutes, giving 5
+    except:
+        d.driver.quit()
+        raise
+
+def test_3ccp4build():
+    try:
+        verifyCCP4Build(d.driver, 1200, '0006', 0.26, 0.29) # run takes long
+        sf.renameProject(d.driver, d.testName)
+        d.driver.quit()
+    except:
+        d.driver.quit()
+        raise
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.set_defaults(auto=False)
+    parser.add_argument('--remote', action='store', dest='remote', help=argparse.SUPPRESS, default='') # run tests on Selenium Server hub. Contains hub URL
+    parser.add_argument('--browser', action='store', dest='browser', help=argparse.SUPPRESS, default='Firefox') # Firefox or Chrome
+    parser.add_argument('--cloud', action='store', dest='cloud', help=argparse.SUPPRESS, default='http://ccp4serv6.rc-harwell.ac.uk/jscofe-dev/') # Cloud URL
+    parser.add_argument('--login', action='store', dest='login', help=argparse.SUPPRESS, default='setests') # Login
+    parser.add_argument('--password', action='store', dest='password', help=argparse.SUPPRESS, default='cloud8testS') # password
+    parser.add_argument('--nologin', action='store', dest='nologin', help=argparse.SUPPRESS, default=False) # login into Cloud or not
+
+    parameters = parser.parse_args(sys.argv[1:])
+
+    test_1RefmacBasic(browser=parameters.browser,  # or 'Chrome'
+               cloud=parameters.cloud,
+               nologin=parameters.nologin,  # True for Cloud Desktop (no login page), False for remote server that requires login.
+               login=parameters.login,  # Used to login into remote Cloud
+               password=parameters.password,  # Used to login into remote Cloud
+               remote=parameters.remote  # 'http://130.246.213.187:4444/wd/hub' for Selenium Server hub
+               )
+    test_2buccaneer()
+    test_3ccp4build()
