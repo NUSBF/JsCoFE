7e735dd3f9a3f1011f5075eae9933bb3b964feb8
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2020-11-14 19:43:17 +0000

    fixes

--- pycofe/apps/ccp4build/ccp4build_parrot.py
+++ pycofe/apps/ccp4build/ccp4build_parrot.py
@@ -103,11 +103,11 @@ class Parrot(ccp4build_base.Base):
         if meta["xyzpath"]:
             self.write_script ([ "pdbin-mr " + meta["xyzpath"] ])
             if firstrun and self.input_data["mode"]=="EP":
-                cycles = 5
+                cycles = 8
         elif meta["xyzpath_ha"]:
             self.write_script ([ "pdbin-ha " + meta["xyzpath_ha"] ])
             if firstrun and self.input_data["mode"]=="EP":
-                cycles = 5
+                cycles = 8
 
         self.write_script ([ "cycles " + str(cycles) ])
 
