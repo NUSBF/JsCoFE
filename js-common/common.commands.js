361b246ef2ea48de888e1d56975f6cc114083382
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2020-11-25 14:57:54 +0000

    docs and job archiving fixes

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -2,7 +2,7 @@
 /*
  *  ===========================================================================
  *
- *    24.11.20   <--  Date of Last Modification.
+ *    25.11.20   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  ---------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.6.017 [24.11.2020]';
+var jsCoFE_version = '1.6.017 [25.11.2020]';
 
 function appVersion()  {
   return jsCoFE_version;
