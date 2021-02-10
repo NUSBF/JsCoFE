7f19bf3d961acae5e3ba4b60e8717cf014e64ddd
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2021-02-10 22:32:11 +0000

    fixes

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -2,7 +2,7 @@
 /*
  *  ===========================================================================
  *
- *    09.02.21   <--  Date of Last Modification.
+ *    10.02.21   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.6.019 [09.02.2021]';
+var jsCoFE_version = '1.6.019 [10.02.2021]';
 
 function appVersion()  {
   return jsCoFE_version;
