71d21c0066200a0aa33aa26c893956e945e3673a
krissinel <eugene.krissinel@stfc.ac.uk>
2021-07-28 17:49:39 +0100

    datestamp

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -25,8 +25,8 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.6.023 [27.07.2021]';
-// var jsCoFE_version = '1.6.022 [24.05.2021]';
+// var jsCoFE_version = '1.6.023 [27.07.2021]';
+var jsCoFE_version = '1.6.022 [24.05.2021]';
 
 function appVersion()  {
   return jsCoFE_version;
