8b4d0d24e57fe6695f70bf6a923c5f89acf1821c
urp21839 <oleg.kovalevskiy@stfc.ac.uk>
2021-11-29 08:43:37 +0000

    Wokflows: added printout to the main log for debugging

--- pycofe/auto/auto_api.py
+++ pycofe/auto/auto_api.py
@@ -70,12 +70,14 @@ def addTask ( taskName,taskClassName,parentName ):
     task.fields     = jsonut.jObject()
     task.parentName = parentName
     auto_meta.set_field ( taskName,task )
+    log('calling addTask: "%s", "%s", "%s"' % (taskName,taskClassName,parentName))
     return
 
 def noteTask ( taskName,notedName ):
     task = auto_meta.get_field ( taskName )
     if task:
         auto_meta.context.tasks.set_field ( notedName,task )
+        log('calling noteTask: "%s", "%s"' % (taskName, notedName))
         return True
     return False
 
@@ -95,6 +97,7 @@ def cloneTask ( clonedTaskName,notedName ):
             task.fields.set_field ( key,originalTask.fields.get_field(key) )
         task.parentName = originalTask.parentName
         auto_meta.set_field ( clonedTaskName,task )
+        log('calling cloneTask: "%s", "%s"' % (clonedTaskName, notedName))
         return True
     return False
 
@@ -110,6 +113,7 @@ def addTaskData ( taskName,inputId,dataClass ):
                 task.data.get_field(inputId).append ( dataClass[i] )
         else:
             task.data.get_field(inputId).append ( dataClass )
+        log('calling addTaskData: "%s", "%s", "%s"' % (taskName, inputId, dataClass))
     return
 
 def addTaskParameter ( taskName,parameterName,parameterValue ):
@@ -117,6 +121,7 @@ def addTaskParameter ( taskName,parameterName,parameterValue ):
     task = auto_meta.get_field ( taskName )
     if task:
         task.parameters.set_field ( parameterName,parameterValue )
+        log('calling addTaskParameter: "%s", "%s", "%s"' % (taskName, parameterName, parameterValue))
     return
 
 def addTaskField ( taskName,fieldName,fieldValue ):
@@ -124,16 +129,19 @@ def addTaskField ( taskName,fieldName,fieldValue ):
     task = auto_meta.get_field ( taskName )
     if task:
         task.fields.set_field ( fieldName,fieldValue )
+        log('calling addTaskField: "%s", "%s", "%s"' % (taskName, fieldName, fieldValue))
     return
 
 def addContext ( contextName,context ):
     global auto_meta
     auto_meta.context.custom.set_field ( contextName,context )
+    log('calling addContext: "%s", "%s"' % (contextName, context))
     return
 
 def getContext ( contextName ):
     global auto_meta
-    return auto_meta.context.custom.get_field ( contextName );
+    log('calling getContext: "%s"' % (contextName))
+    return auto_meta.context.custom.get_field ( contextName )
 
 # def loadContext():
 #     global auto_meta
