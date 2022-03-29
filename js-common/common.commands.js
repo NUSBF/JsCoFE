bd8086e592e68e7d691ea3448b013e790a90f2d6
krissinel <eugene.krissinel@stfc.ac.uk>
2022-03-29 15:30:35 +0100

    deposition task fix

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -2,7 +2,7 @@
 /*
  *  ===========================================================================
  *
- *    27.03.22   <--  Date of Last Modification.
+ *    29.03.22   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.7.002 [27.03.2022]';
+var jsCoFE_version = '1.7.002 [29.03.2022]';
 // var jsCoFE_version = '1.7.001 [26.11.2021]';
 
 function appVersion()  {
