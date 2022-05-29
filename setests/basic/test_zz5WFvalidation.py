53369540620081557245261c4b07a7213a12a890
Maria Fando <b_with_d@outlook.com>
2022-05-29 22:17:11 +0000

    Upload New File

---
+++ setests/basic/test_zz5WFvalidation.py
@@ -0,0 +1,111 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+import time, sys, os, re
+
+curPath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+if curPath not in sys.path:
+    sys.path.insert(0, curPath)
+import setests_func as sf
+
+d = sf.driverHandler()
+
+def verifyModelcraft(driver, waitLong, jobNumber, targetRwork, targetRfree):
+    rWork = 1.0
+    rFree = 1.0
+    print('Modelcraft verification, job ' + jobNumber)
+
+    time.sleep(3)
+    startTime = time.time()
+
+    while (True):
+        ttts = sf.tasksTreeTexts(driver)
+        for taskText in ttts:
+            # Job number as string
+            match = re.search('\[' + jobNumber + '\] modelcraft -- Compl=(.*)%.*R=(0\.\d*).*Rfree=(0\.\d*)', taskText)
+            if match:
+                rWork = float(match.group(2))
+                rFree = float(match.group(3))
+                break
+        if (rWork != 1.0) or (rFree != 1.0):
+            break
+        curTime = time.time()
+        if curTime > startTime + float(waitLong):
+            print('*** Timeout for Modelcraft results! Waited for %d seconds.' % waitLong)
+            break
+        time.sleep(3)
+
+    if (rWork == 1.0) or (rFree == 1.0):
+        print('*** Verification: could not find Rwork or Rfree value after Modelcraft run')
+    else:
+        print('*** Verification: Modelcraft Rwork is %0.4f (expecting <%0.2f), Rfree is %0.4f (expecing <%0.2f)' % (
+            rWork, targetRwork, rFree, targetRfree))
+    assert rWork < targetRwork
+    assert rFree < targetRfree
+
+    return ()
+
+def test_cloneBasic(browser,
+                     cloud,
+                     nologin,
+                     login,
+                     password,
+                     remote
+                     ):
+
+    (d.driver, d.waitLong, d.waitShort) = sf.startBrowser(remote, browser)
+    d.browser = browser
+    d.cloud = cloud
+    d.nologin = nologin
+    d.password = password
+    d.remote = remote
+    d.login = login
+
+    d.testName = 'clonedTest'
+
+    try:
+        print('Opening URL: %s' % cloud)
+        d.driver.get(cloud)
+        assert "CCP4 Cloud" in d.driver.title
+
+        if not nologin:
+            sf.loginToCloud(d.driver, login, password)
+        
+        sf.enterProject(d.driver, 'clonedTest')
+        sf.verifyBuccaneer(d.driver, d.waitLong, '0006', 0.3, 0.35)
+        verifyModelcraft(d.driver, d.waitLong, '0005', 0.3, 0.3)
+        sf.renameProject(d.driver, 'Cloned_cloneTest')
+
+        d.driver.quit()
+
+    except:
+        d.driver.quit()
+        raise
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.set_defaults(auto=False)
+    parser.add_argument('--remote', action='store', dest='remote', help=argparse.SUPPRESS, default='') # run tests on Selenium Server hub. Contains hub URL
+    parser.add_argument('--browser', action='store', dest='browser', help=argparse.SUPPRESS, default='Firefox') # Firefox or Chrome
+    parser.add_argument('--cloud', action='store', dest='cloud', help=argparse.SUPPRESS, default='http://ccp4serv6.rc-harwell.ac.uk/jscofe-dev/') # Cloud URL
+    parser.add_argument('--login', action='store', dest='login', help=argparse.SUPPRESS, default='setests') # Login
+    parser.add_argument('--password', action='store', dest='password', help=argparse.SUPPRESS, default='') # password
+    parser.add_argument('--nologin', action='store', dest='nologin', help=argparse.SUPPRESS, default=False) # login into Cloud or not
+
+    parameters = parser.parse_args(sys.argv[1:])
+
+    test_cloneBasic(browser=parameters.browser,  # or 'Chrome'
+                     cloud=parameters.cloud,
+                     nologin=parameters.nologin,  # True for Cloud Desktop (no login page), False for remote server that requires login.
+                     login=parameters.login,  # Used to login into remote Cloud
+                     password=parameters.password,  # Used to login into remote Cloud
+                     remote=parameters.remote  # 'http://130.246.213.187:4444/wd/hub' for Selenium Server hub
+                     )
