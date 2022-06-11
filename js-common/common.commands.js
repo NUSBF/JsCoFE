094b67db83d86ada415a1e91072e2c61376ee028
krissinel <eugene.krissinel@stfc.ac.uk>
2022-06-11 16:02:40 +0100

    folder fix

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -1,7 +1,7 @@
 /*
  *  ===========================================================================
  *
- *    09.06.22   <--  Date of Last Modification.
+ *    11.06.22   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -24,7 +24,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.7.004 [09.06.2022]';
+var jsCoFE_version = '1.7.004 [11.06.2022]';
 
 function appVersion()  {
   return jsCoFE_version;
