be83dd8db34b989d09b950db74761d1765112962
krissinel <eugene.krissinel@stfc.ac.uk>
2022-06-12 22:50:38 +0100

    fixes

--- pycofe/tasks/structureprediction.py
+++ pycofe/tasks/structureprediction.py
@@ -146,9 +146,9 @@ class StructurePrediction(basic.TaskDriver):
                 if PAE_png:
                     self.putMessage ( "<h3>PAE matrices</h3>" )
                     self.putMessage1 ( self.report_page_id(),"<img src=\"" + PAE_png +\
-                                "\" height=\"200px\" style=\"position:relative; left:-" +\
-                                str(35*(len(fpaths)-1)) + "px;\"/>",
-                                self.rvrow,col=0,rowSpan=1,colSpan=3 )
+                                "\" height=\"200px\" style=\"position:relative; left:" +\
+                                str(35*(1-len(fpaths))) + "px;\"/>",
+                                self.rvrow,col=0,rowSpan=1,colSpan=1 )
                     self.rvrow += 1
                 if plddt_png:
                     self.putMessage ( "<h3>PLLDT scores</h3>" )
