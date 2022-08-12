c6bf5c8d816c5a0c0052284ece288812c0c3365d
krissinel <eugene.krissinel@stfc.ac.uk>
2022-08-12 15:54:14 +0100

    date stamp

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -1,7 +1,7 @@
 /*
  *  ===========================================================================
  *
- *    11.08.22   <--  Date of Last Modification.
+ *    12.08.22   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.7.006 [11.08.2022]';
+var jsCoFE_version = '1.7.006 [12.08.2022]';
 
 function appVersion()  {
   return jsCoFE_version;
