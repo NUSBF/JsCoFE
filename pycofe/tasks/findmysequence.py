0f7d3f2161f0ee06eb72a8d8abcb55515e00d30b
krissinel <eugene.krissinel@stfc.ac.uk>
2022-12-07 20:28:49 +0000

    findmysequence

--- pycofe/tasks/findmysequence.py
+++ pycofe/tasks/findmysequence.py
@@ -1,18 +1,16 @@
 ##!/usr/bin/python
 
-# not python-3 ready
-
 #
 # ============================================================================
 #
-#    23.05.21   <--  Date of Last Modification.
+#    07.12.22   <--  Date of Last Modification.
 #                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ----------------------------------------------------------------------------
 #
-#  LORESTR EXECUTABLE MODULE
+#  FINDMYSEQUENCE EXECUTABLE MODULE
 #
 #  Command-line:
-#     ccp4-python -m pycofe.tasks.lorestr jobManager jobDir jobId
+#     ccp4-python -m pycofe.tasks.findmysequence jobManager jobDir jobId
 #
 #  where:
 #    jobManager  is either SHELL or SGE
@@ -21,32 +19,33 @@
 #                       all successful imports
 #      jobDir/report  : directory receiving HTML report
 #
-#  Copyright (C) Eugene Krissinel, Oleg Kovalevskiy, Andrey Lebedev 2017-2021
+#  Copyright (C) Grzegorz Chojnowski, Eugene Krissinel, Andrey Lebedev 2022
 #
 # ============================================================================
 #
 
 #  python native imports
 import os
-import sys
+# import sys
 import json
-import html
+# import html
+import requests
 
 #  application imports
-from . import basic
-from   pycofe.proc   import qualrep
-from   varut          import signal
-from   pycofe.verdicts  import verdict_lorestr
-from   pycofe.auto   import auto
+from .     import basic
+from varut import signal
+from proc  import import_seqcp
 
-import requests
-from proc import import_seqcp
+# from   pycofe.proc   import qualrep
+# from   pycofe.verdicts  import verdict_lorestr
+# from   pycofe.auto   import auto
+
 
 
 def download_proteome_from_unbiprot(uid="UP000005206", datadir='input'):
 
     ofile = os.path.join(datadir, f"{uid}.fasta.gz")
-    url = f"https://rest.uniprot.org/uniprotkb/stream?format=fasta&query=%28%28proteome%3A{uid}%29%29"
+    # url = f"https://rest.uniprot.org/uniprotkb/stream?format=fasta&query=%28%28proteome%3A{uid}%29%29"
     url = f"https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=fasta&query=%28%28proteome%3A{uid}%29%29"
     request = requests.get(url, stream=True)# as request:
     request.raise_for_status()
@@ -62,35 +61,24 @@ def download_proteome_from_unbiprot(uid="UP000005206", datadir='input'):
 class FindMySequence(basic.TaskDriver):
 
     # redefine name of input script file
-    def file_stdin_path(self):  return "findmysequence.script"
+    # def file_stdin_path(self):  return "findmysequence.script"
+
+    def importDir(self): return "." # in current directory ( job_dir )
 
     # ------------------------------------------------------------------------
 
     def run(self):
 
-        # Just in case (of repeated run) remove the output xyz file. When lorestr
-        # succeeds, this file is created.
-        # if os.path.isfile(self.getXYZOFName()):
-        #     os.remove(self.getXYZOFName())
-
-        # Prepare lorestr input
         # fetch input data
-        # hkl     = self.makeClass ( self.input_data.data.hkl[0] )
         istruct = self.makeClass ( self.input_data.data.istruct[0] )
 
-        # Prepare report parser
-        #self.setGenericLogParser ( self.lorestr_report(),False )
-
-        mtzin = istruct.getMTZFilePath ( self.inputDir() )
+        mtzin   = istruct.getMTZFilePath   ( self.inputDir() )
         modelin = istruct.getMMCIFFilePath ( self.inputDir() )
-        uid = self.getParameter(self.task.parameters.sec1.contains.UPID)
+        uid     = self.getParameter(self.task.parameters.sec1.contains.UPID)
         tophits = self.getParameter(self.task.parameters.sec1.contains.TOPHITS)
-        selstr = self.getParameter(self.task.parameters.sec1.contains.SELSTR)
-
-        labin_ph = [istruct.PHI,istruct.FOM,istruct.FWT,istruct.PHWT]
+        selstr  = self.getParameter(self.task.parameters.sec1.contains.SELSTR)
 
         jsonout = os.path.join(self.outputDir(), 'results.json')
-
 
         cmd=["--mtzin", mtzin, "--labin", f"{istruct.FWT},{istruct.PHWT}",
                 "--modelin", modelin, '--jsonout', jsonout, '--tophits', tophits, "--select", selstr]
@@ -109,8 +97,6 @@ class FindMySequence(basic.TaskDriver):
 
         self.addCitations ( ['findmysequence'] )
 
-        have_results = False
-
         if rc.msg:
             self.putTitle ( "Results" )
             self.putMessage ( "<h3>findMySequence failure</h3>" )
@@ -119,20 +105,41 @@ class FindMySequence(basic.TaskDriver):
         # fetch sequence as a string
 
         with open(jsonout, 'r') as ifile:
-            results=json.loads(ifile.read())
+            results = json.loads(ifile.read())
+
+        nseq = 0
 
         if results:
+            self.putTitle ( "Found sequence matches" )
+
+            seqdata = ""
             for k,v in results.items():
-                self.putMessage ( f"<b>#{k}</b> E-value={v['evalue']}</br>>{v['sequence_id']}</br>{v['sequence']}<\br>" )
+                seqdata += ">" + v['sequence_id'] + "|E-value=" + str(v["evalue"]) +\
+                           "\n" + v["sequence"] + "\n"
+            if seqdata:
+                nseq = import_seqcp.run ( self,"protein",self.outputFName,seqdata )
+            else:
+                self.putMessage ( "<b>No results found.</b> <i>This is likely to be a program bug, please report.</i>" )
+
+            # for k,v in results.items():
+            #     import_seqcp.run ( self,
+            #         "protein",
+            #         self.outputFName,
+            #         ">" + v['sequence_id'] + "|E-value=" + v["evalue"] + "\n" +  v["sequence"] 
+            #     )
+
+            # for k,v in results.items():
+            #     self.putMessage ( f"<b>#{k}</b> E-value={v['evalue']}</br>>{v['sequence_id']}</br>{v['sequence']}<\br>" )
         else:
-            self.putMessage ( "<h3> No plausible sequence matches found </h3>" )
+            self.putTitle ( "No plausible sequence matches found" )
 
-        # sequence = myfunction() 
 
-        # self.stdoutln ( "\n Found sequence = " + sequence )
+        self.generic_parser_summary["pisa"] = {
+            "summary_line" : str(nseq) + " sequences found"
+        }
 
         # close execution logs and quit
-        self.success ( have_results )
+        self.success ( (nseq>0) )
         return
 
 
