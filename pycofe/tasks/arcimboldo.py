72cc65f1bc3f45c5bfb2c71007e69a5e7faf102a
krissinel <eugene.krissinel@stfc.ac.uk>
2022-12-08 13:19:36 +0000

    arcimboldo cleanup

--- pycofe/tasks/arcimboldo.py
+++ pycofe/tasks/arcimboldo.py
@@ -634,11 +634,11 @@ class Arcimboldo(basic.TaskDriver):
 
         # unless cleaned up, symbolic links inside this directory will not let
         # it to be sent back to FE.
-        dlist = os.listdir(self.arcimboldoDir())
-        for dname in dlist:
-            fpath = os.path.join(self.arcimboldoDir(), dname)
-            if os.path.isdir(fpath):
-                shutil.rmtree(fpath)
+        # dlist = os.listdir(self.arcimboldoDir())
+        # for dname in dlist:
+        #     fpath = os.path.join(self.arcimboldoDir(), dname)
+        #     if os.path.isdir(fpath):
+        #         shutil.rmtree(fpath)
 
         shutil.rmtree ( self.arcimboldoDir() )
 
