a1b9455d8b616d4162625f9e54bd23c38091a9d4
krissinel <eugene.krissinel@stfc.ac.uk>
2022-12-14 23:35:17 +0000

    version advance

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.7.008 [14.12.2022]';
+var jsCoFE_version = '1.7.009 [14.12.2022]';
 
 function appVersion()  {
   return jsCoFE_version;
