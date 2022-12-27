860b956b1a7e365928b88896c2be85371fbf74cf
krissinel <eugene.krissinel@stfc.ac.uk>
2022-12-27 19:16:16 +0000

    fix revision inspect dialog

--- js-common/dtypes/common.dtypes.revision.js
+++ js-common/dtypes/common.dtypes.revision.js
@@ -2,7 +2,7 @@
 /*
  *  ==========================================================================
  *
- *    21.12.22   <--  Date of Last Modification.
+ *    27.12.22   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -196,8 +196,8 @@ if (!__template)  {
     if (this.Structure && this.Substructure)
       dataSummaryPage.makeRow ( 'Revision\'s phases from',phases_source,
                      'Dataset containing phases for using in subsequent tasks' );
-    if ((this.Structure && this.Structure.arePhases()) ||
-        (this.Substructure && this.Substructure.arePhases()))
+    if ((this.Structure && this.Structure.hasPhases()) ||
+        (this.Substructure && this.Substructure.hasPhases()))
       dataSummaryPage.makeRow ( 'Phases\' type',phases_type,
                      'Type of phasing method used to calculate revision\'s phases' );
     tab1.grid.setWidget ( dataSummaryPage, 1,0,1,1 );
