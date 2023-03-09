a384e6f3d793670a18e35794deeeba96f3a3e3ef
Maria Fando <b_with_d@outlook.com>
2023-03-09 12:42:47 +0000

    setests: mrbump test updated

--- setests/basic/test_mrbump.py
+++ setests/basic/test_mrbump.py
@@ -47,7 +47,7 @@ def mrbumpAfterASU(driver, waitLong):
             break
 
     try:
-        wait = WebDriverWait(driver, 600) # allowing 10 minutes to the task to finish
+        wait = WebDriverWait(driver, 800) # allowing 10 minutes to the task to finish
         # Waiting for the text 'completed' in the ui-dialog-title of the task [0003]
         wait.until(EC.presence_of_element_located
                    ((By.XPATH,"//*[@class='ui-dialog-title' and contains(text(), 'completed') and contains(text(), '[0003]')]")))
