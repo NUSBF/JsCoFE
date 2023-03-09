e30840317a08841f8bd3f6779da155da168947bf
krissinel <eugene.krissinel@stfc.ac.uk>
2023-03-09 13:38:25 +0000

    input data order in job dialogs fix

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -1,7 +1,7 @@
 /*
  *  ===========================================================================
  *
- *    07.03.23   <--  Date of Last Modification.
+ *    09.03.23   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -25,7 +25,7 @@
 
 function appName()  { return 'CCP4 Cloud' }  // application name for reporting
 
-var jsCoFE_version = '1.7.011 [07.03.2023]';
+var jsCoFE_version = '1.7.011 [09.03.2023]';
 
 function appVersion()  {
   return jsCoFE_version;
