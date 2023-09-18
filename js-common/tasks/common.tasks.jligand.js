3728e4ee607fa4d27c9b4f1ceb771a7aa26a76a5
Maria Fando <b_with_d@outlook.com>
2023-09-18 11:12:24 +0000

    Revert "jligand"
    This reverts commit db2c4ae33f9bcc86eb69bbb8badfab8c7461ccf8

--- js-common/tasks/common.tasks.jligand.js
+++ js-common/tasks/common.tasks.jligand.js
@@ -47,12 +47,6 @@ function TaskJLigand()  {
       // force       : 1,          // "show" all revisions available
       min         : 1,          // minimum acceptable number of data instances
       max         : 1           // maximum acceptable number of data instances
-    },{
-      data_type   : {'DataLigand':[]},  // data type(s) and subtype(s)
-      label       : 'Ligand data', // label for input dialog
-      inputId     : 'ligand',      // input Id for referencing input fields
-      min         : 0,             // minimum acceptable number of data instances
-      max         : 19             // maximum acceptable number of data instances
     }
   ];
 
