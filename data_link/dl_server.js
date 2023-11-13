ed4bcaddc2031c56ee1b92be3aa012c4b8613705
Jools Wills <jools.wills@stfc.ac.uk>
2023-11-13 08:50:23 +0000

    Initial version of the data link service
    Data Link is a web service with REST API, that can retrieve raw x-ray diffraction images for temporary storage and processing in CCP4 Cloud.
    
    It can currently retrieve data from the following public archives:
    
    - https://xrda.pdbj.org/ - The Xtal Raw Data Archive from the Protein Data Bank Japan.
    - https://data.sbgrid.org/ - SBGrid Data Bank from the SBGrid consortium.
    - https://proteindifftraction.org/ - Integrated Resource for Reproducibility in Macromolecular Crystallography from the National Institute of Health.
    
    In the future it is planned to add support for linking it it to data storage and data producing facilities, such as ICAT @ STFC/SCD, synchrotrons and in-house X-ray instruments (e.g. Bruker diffractometers)
    
    Currently the API can only pull data from external sources. The ability to push data to the API will be added later which should help with linking to data producing facilities.
    
    This commit includes the following files:
    
    dl_server.js:
    The web server - built on Express
    
    js/data_link.js:
    Main class handling the local catalog, and passing requests to the data sources
    
    js/data_source.js:
    Main class for raw data sources, which they extend.
    
    js/irrmc.js:
    Data source for aquiring diffraction images from https://proteindiffraction.org/
    
    js/pdbj.js:
    Data source for aquiring diffraction images from https://xrda.pdbjbk1.pdbj.org/
    
    js/sbgrid.js:
    Data source for aquiring diffraction images from https://data.sbgrid.org/
    
    Node modules required are cheerio rsync2 express pino

---
+++ data_link/dl_server.js
@@ -0,0 +1,149 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const logger = require('pino-http')
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+//app.use(logger());
+
+const { tools, status } = require('./js/tools.js');
+const datalink = require('./js/data_link.js');
+
+const PORT = 8000;
+
+class server {
+
+  constructor() {
+    this.datalink = new datalink();
+  }
+
+  jsonResponse(res, data) {
+    res.set('Content-Type', 'application/json');
+    res.send(tools.jsonMessage(data));
+  }
+
+  middleware(req, res, next) {
+    next();
+  }
+
+  checkCloudId(req, res, next) {
+    if (tools.validCloudId(req.params.user, req.body.cloud_id)) {
+      next();
+    } else {
+      this.jsonResponse(res, tools.errorMsg('Invalid User or Cloud Run ID'));
+    }
+  }
+
+  getSources(req, res) {
+    this.jsonResponse(res, this.datalink.getSources());
+  }
+
+  getSourceCatalog(req, res) {
+    let response = {};
+    if (req.params.id) {
+      response = this.datalink.getSourceCatalog(req.params.id);
+      if (response.error) {
+        res.status(404).send(response);
+        return;
+      }
+    } else {
+      response = this.datalink.getAllSourceCatalogs();
+    }
+    this.jsonResponse(res, response);
+  }
+
+  searchSourceCatalog(req, res) {
+    this.jsonResponse(res, this.datalink.searchSourceCatalog(req.params.id.toLowerCase()));
+  }
+
+  updateSourceCatalog(req, res) {
+    let response = {};
+    if (req.params.id) {
+      response = this.datalink.updateSourceCatalog(req.params.id);
+    } else {
+      response = this.datalink.updateAllSourceCatalogs();
+    }
+    this.jsonResponse(res, response);
+  }
+
+  getLocalCatalog(req, res) {
+    this.jsonResponse(res, this.datalink.catalog);
+  }
+
+  dataAquire(req, res) {
+    let force = false;
+    if (req.query.force == 1) {
+      force = true;
+    }
+    this.jsonResponse(res, this.datalink.dataAquire(req.params.user, req.params.source, req.params.id, force));
+  }
+
+  dataStatus(req, res) {
+    this.jsonResponse(res, this.datalink.dataStatus(req.params.user, req.params.source, req.params.id));
+  }
+
+  dataRemove(req, res) {
+    this.jsonResponse(res, this.datalink.dataRemove(req.params.user, req.params.source, req.params.id));
+  }
+
+  dataInUse(req, res) {
+    this.jsonResponse(res, this.datalink.dataInUse(req.params.user, req.params.source, req.params.id, req.params.value));
+  }
+
+  start(port) {
+    app.use((req, res, next) => this.middleware(req, res, next));
+
+    app.use(bodyparser.json());
+
+    // app.use(bodyparser.urlencoded({ extended: true }));
+
+    // data source info/catalog endpoints
+    app.get(['/sources'], (req, res) => this.getSources(req, res) );
+    app.get(['/source/catalog', '/source/catalog/:id'], (req, res) => this.getSourceCatalog(req, res) );
+    app.get(['/source/search/:id'], (req, res) => this.searchSourceCatalog(req, res) );
+
+    app.put(['/source/update', '/source/update/:id'], (req, res) => this.updateSourceCatalog(req, res) );
+
+    // local catalog endpoints
+    app.get(['/local/catalog'], (req, res) => this.getLocalCatalog(req, res) );
+
+    // data retrieval endpoints
+    app.put(['/data/aquire/:user/:source/:id'],
+      (req, res, next) => this.checkCloudId(req, res, next),
+      (req, res) => this.dataAquire(req, res) );
+    app.put(['/data/remove/:user/:source/:id'],
+      (req, res, next) => this.checkCloudId(req, res, next),
+      (req, res) => this.dataRemove(req, res) );
+    app.put(['/data/status/:user', '/data/status/:user/:source/:id', ],
+      (req, res, next) => this.checkCloudId(req, res, next),
+      (req, res) => this.dataStatus(req, res) );
+
+    app.patch(['/data/inuse/:value/:user/:source/:id'],
+      (req, res, next) => this.checkCloudId(req, res, next),
+      (req, res) => this.dataInUse(req, res) );
+
+    app.get('/', function(req,res) {
+      res.send(app._router.stack.map( r => r.route?.path ));
+    });
+
+    app.listen(port, function(e) {
+      if (e) console.log('Error: ' +  e)
+      console.log('Listening on Port', port);
+    });
+  }
+
+  stop() {
+    server.close(() => {
+        console.log('Shutting down.');
+        process.exit(0);
+    });
+  }
+
+}
+
+server = new server();
+server.start(PORT);
---
+++ data_link/js/sources/sbgrid.js
@@ -0,0 +1,103 @@
+// pdbj data source
+
+'use strict';
+
+const fs = require('fs');
+const logger = require('pino')();
+const cheerio = require('cheerio');
+
+const dataSource = require('../data_source.js');
+const { tools } = require('../tools.js');
+
+const URL_CAT = 'https://data.sbgrid.org/data/';
+const URL_RSYNC = 'data.sbgrid.org::10.15785/SBGRID/';
+
+const RE_PDB = /structureId=([^\"]+)/;
+const RE_PROJ = /dataset\/([^\"\/]+)/;
+
+class sbgrid extends dataSource {
+
+  description = 'The SBGrid Data Bank';
+  url = 'https://data.sbgrid.org'
+
+  async getCatalog() {
+    let entries = {};
+    let pages, page = 1;
+    logger.info(`${this.name} - Scraping entries...`);
+    while (true) {
+      let url = URL_CAT + '?page=' + page;
+      logger.info(url);
+
+      let pdbid = [];
+      let data = [];
+
+      let html = await tools.httpRequest(url);
+      let dom = cheerio.load(html);
+
+      // get number of pages
+      if (page == 1) {
+        pages = dom('ul.paginator li a.page').last().text();
+      }
+
+      // loop through each entry
+      let group = dom('.media-body');
+
+      group.each((i, elem) => {
+        let found, proj, pdb, doi;
+
+        // get entry description
+        let desc = dom(elem).find('h3').text();
+
+        // find the links
+        let links = dom(elem).find('a');
+        for (let j = 0; j < links.length; j++) {
+          let href = links[j].attribs.href;
+
+          // extract the project id
+          found = href.match(RE_PROJ);
+          if (found) {
+            proj = found[1].toLowerCase();
+            doi = links[j].children[0].data;
+          }
+
+          // extract the PDB id
+          found = href.match(RE_PDB);
+          if (found) {
+            pdb = found[1].toLowerCase();
+          }
+
+          // if there is no pdb id found, use the project id
+          if (! pdb) {
+            pdb = proj;
+          }
+
+        }
+
+        entries[proj] = {};
+        entries[proj].path = proj;
+        entries[proj].pdb = pdb;
+        entries[proj].desc = desc;
+        entries[proj].doi = doi;
+
+      });
+
+      if (page == pages) {
+        break;
+      }
+
+      page ++;
+    }
+    this.saveCatalog(entries);
+  }
+
+  getCatalogOld() {
+    this.rsyncGetCatalog(URL_RSYNC);
+  }
+
+  getData(user, id, catalog) {
+    this.rsyncGetData(URL_RSYNC, user, id, catalog);
+  }
+
+}
+
+module.exports = sbgrid;
