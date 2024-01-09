0fa0cbb9fada16abf857d276f0b0231644eaa696
Maria Fando <b_with_d@outlook.com>
2024-01-09 11:51:47 +0000

    test(setests): tests update

--- setests/basic/test_4dpl_wf.py
+++ setests/basic/test_4dpl_wf.py
@@ -40,9 +40,9 @@ def startDPL_mdm2(driver, dirName, waitShort=90):
         pdbFileName = os.path.join(dirName, 'mdm2_nolig', '4hg7_nolig.pdb')
         cifFileName = os.path.join(dirName, 'mdm2_nolig', 'nut.cif')
         projectInputs = driver.find_elements_by_xpath("//input[contains(@id,'input') and @type='file' and contains(@name,'uploads[]')]")
-        projectInputs[-3].send_keys(mtzFileName)
-        projectInputs[-2].send_keys(pdbFileName)
-        projectInputs[-1].send_keys(cifFileName)
+        projectInputs[-5].send_keys(mtzFileName)
+        projectInputs[-4].send_keys(pdbFileName)
+        projectInputs[-3].send_keys(cifFileName)
 
     time.sleep(2)
     sf.clickByXpath(driver, "//button[normalize-space()='Apply & Upload']")
