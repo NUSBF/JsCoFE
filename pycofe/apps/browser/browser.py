ff68e2571b08881026bc2264688e8fa23373dcaf
krissinel <eugene.krissinel@stfc.ac.uk>
2024-02-25 18:47:45 +0000

    save

--- pycofe/apps/browser/browser.py
+++ pycofe/apps/browser/browser.py
@@ -1,4 +1,8 @@
 
+
+
+
+
 import argparse
 
 from   PySide2.QtCore import *
@@ -28,6 +32,7 @@ import sys
 class CustomWebEnginePage(QWebEnginePage):
     # Store second window.
     external_window = None
+
     def acceptNavigationRequest(self, url,  _type, isMainFrame):
         print( " >>>>> ",url, _type, isMainFrame)
         if _type == QWebEnginePage.NavigationTypeLinkClicked:
@@ -39,171 +44,174 @@ class CustomWebEnginePage(QWebEnginePage):
         return super().acceptNavigationRequest(url,  _type, isMainFrame)
 
     def javaScriptConsoleMessage ( self,level,message,lineNumber,sourceID):
-        print( " %% ",sourceID,":",lineNumber, ": ",message )
+        print ( " % ",sourceID,":",lineNumber )
+        print ( " >> ",message )
         return
+		
+    def createWindow ( self,type ):
+        return super().acceptNavigationRequest(url,  _type, isMainFrame)
 
 
 # creating main window class
 class MainWindow(QMainWindow):
 
-	# constructor
-	def __init__ ( self,args ):
-		super(MainWindow, self).__init__()
+		# constructor
+		def __init__ ( self,args ):
+				super(MainWindow, self).__init__()
 
-		self.urlbar = None
-		self.args   = args
+				self.urlbar = None
+				self.args   = args
 
-		# creating a QWebEngineView
-		self.browser = QWebEngineView()
-		self.browser.setPage(CustomWebEnginePage(self))
+				# creating a QWebEngineView
+				self.browser = QWebEngineView()
+				self.browser.setPage(CustomWebEnginePage(self))
 
-		self.settings = QSettings ("CCP4","CCP4 Browser")
+				self.settings = QSettings ("CCP4","CCP4 Browser")
 
-		geometry = self.settings.value('geometry','')
-		if geometry:
-			self.restoreGeometry(geometry)
-		else:
-			self.setGeometry ( 100,100,1200,800 )
+				geometry = self.settings.value('geometry','')
+				if geometry:
+  				  self.restoreGeometry ( geometry )
+        else:
+            self.setGeometry ( 100,100,1200,800 )
 
-		# setting default browser url as google
-		self.browser.setUrl ( QUrl(self.args.url) )
+				# setting default browser url as google
+        self.browser.setUrl ( QUrl(self.args.url) )
 
-		# adding action when url get changed
-		self.browser.urlChanged.connect(self.update_urlbar)
+				# adding action when url get changed
+				self.browser.urlChanged.connect(self.update_urlbar)
 
-		# adding action when loading is finished
-		self.browser.loadFinished.connect(self.update_title)
+				# adding action when loading is finished
+				self.browser.loadFinished.connect(self.update_title)
 
-		# set this browser as central widget or main window
-		self.setCentralWidget ( self.browser )
-
-
-		'''
-		# creating a status bar object
-		self.status = QStatusBar()
-
-		# adding status bar to the main window
-		self.setStatusBar(self.status)
-
-		# creating QToolBar for navigation
-		navtb = QToolBar("Navigation")
-
-		# adding this tool bar tot he main window
-		self.addToolBar(navtb)
-
-		# adding actions to the tool bar
-		# creating a action for back
-		back_btn = QAction("Back", self)
-
-		# setting status tip
-		back_btn.setStatusTip("Back to previous page")
-
-		# adding action to the back button
-		# making browser go back
-		back_btn.triggered.connect(self.browser.back)
-
-		# adding this action to tool bar
-		navtb.addAction(back_btn)
-
-		# similarly for forward action
-		next_btn = QAction("Forward", self)
-		next_btn.setStatusTip("Forward to next page")
-
-		# adding action to the next button
-		# making browser go forward
-		next_btn.triggered.connect(self.browser.forward)
-		navtb.addAction(next_btn)
-
-		# similarly for reload action
-		reload_btn = QAction("Reload", self)
-		reload_btn.setStatusTip("Reload page")
-
-		# adding action to the reload button
-		# making browser to reload
-		reload_btn.triggered.connect(self.browser.reload)
-		navtb.addAction(reload_btn)
-
-		# similarly for home action
-		home_btn = QAction("Home", self)
-		home_btn.setStatusTip("Go home")
-		home_btn.triggered.connect(self.navigate_home)
-		navtb.addAction(home_btn)
-
-		# adding a separator in the tool bar
-		navtb.addSeparator()
-
-		# creating a line edit for the url
-		self.urlbar = QLineEdit()
-
-		# adding action when return key is pressed
-		self.urlbar.returnPressed.connect(self.navigate_to_url)
-
-		# adding this to the tool bar
-		navtb.addWidget(self.urlbar)
-
-		# adding stop action to the tool bar
-		stop_btn = QAction("Stop", self)
-		stop_btn.setStatusTip("Stop loading current page")
-
-		# adding action to the stop button
-		# making browser to stop
-		stop_btn.triggered.connect(self.browser.stop)
-		navtb.addAction(stop_btn)
-		'''
-
-		# showing all the components
-		self.show()
+				# set this browser as central widget or main window
+				self.setCentralWidget ( self.browser )
 
 
-	# method for updating the title of the window
-	def update_title(self):
-		title = self.browser.page().title()
-		# self.setWindowTitle("% s - Geek Browser" % title)
-		self.setWindowTitle ( title )
+				# '''
+				# # creating a status bar object
+				# self.status = QStatusBar()
+
+				# # adding status bar to the main window
+				# self.setStatusBar(self.status)
+
+				# # creating QToolBar for navigation
+				# navtb = QToolBar("Navigation")
+
+				# # adding this tool bar tot he main window
+				# self.addToolBar(navtb)
+
+				# # adding actions to the tool bar
+				# # creating a action for back
+				# back_btn = QAction("Back", self)
+
+				# # setting status tip
+				# back_btn.setStatusTip("Back to previous page")
+
+				# # adding action to the back button
+				# # making browser go back
+				# back_btn.triggered.connect(self.browser.back)
+
+				# # adding this action to tool bar
+				# navtb.addAction(back_btn)
+
+				# # similarly for forward action
+				# next_btn = QAction("Forward", self)
+				# next_btn.setStatusTip("Forward to next page")
+
+				# # adding action to the next button
+				# # making browser go forward
+				# next_btn.triggered.connect(self.browser.forward)
+				# navtb.addAction(next_btn)
+
+				# # similarly for reload action
+				# reload_btn = QAction("Reload", self)
+				# reload_btn.setStatusTip("Reload page")
+
+				# # adding action to the reload button
+				# # making browser to reload
+				# reload_btn.triggered.connect(self.browser.reload)
+				# navtb.addAction(reload_btn)
+
+				# # similarly for home action
+				# home_btn = QAction("Home", self)
+				# home_btn.setStatusTip("Go home")
+				# home_btn.triggered.connect(self.navigate_home)
+				# navtb.addAction(home_btn)
+
+				# # adding a separator in the tool bar
+				# navtb.addSeparator()
+
+				# # creating a line edit for the url
+				# self.urlbar = QLineEdit()
+
+				# # adding action when return key is pressed
+				# self.urlbar.returnPressed.connect(self.navigate_to_url)
+
+				# # adding this to the tool bar
+				# navtb.addWidget(self.urlbar)
+
+				# # adding stop action to the tool bar
+				# stop_btn = QAction("Stop", self)
+				# stop_btn.setStatusTip("Stop loading current page")
+
+				# # adding action to the stop button
+				# # making browser to stop
+				# stop_btn.triggered.connect(self.browser.stop)
+				# navtb.addAction(stop_btn)
+				# '''
+
+				# showing all the components
+				self.show()
 
 
-	# method called by the home action
-	def navigate_home(self):
+    # method for updating the title of the window
+    def update_title(self):
+	    	title = self.browser.page().title()
+		    # self.setWindowTitle("% s - Geek Browser" % title)
+        self.setWindowTitle ( title )
+        return
 
-		# open the google
-		self.browser.setUrl ( QUrl(self.args.url) )
+	  # method called by the home action
+  	# def navigate_home(self):
+	  #   	# open the google
+		#     self.browser.setUrl ( QUrl(self.args.url) )
+		# 		return
 
-	# method called by the line edit when return key is pressed
-	def navigate_to_url(self):
+  	# method called by the line edit when return key is pressed
+  	# def navigate_to_url(self):
 
-		if self.urlbar:
-			# getting url and converting it to QUrl object
-			q = QUrl ( self.urlbar.text() )
+		# 		if self.urlbar:
+		# 				# getting url and converting it to QUrl object
+		# 				q = QUrl ( self.urlbar.text() )
 
-			# if url is scheme is blank
-			if q.scheme() == "":
-				# set url scheme to html
-				q.setScheme("http")
+		# 				# if url is scheme is blank
+		# 				if q.scheme() == "":
+		# 					# set url scheme to html
+		# 					q.setScheme("http")
 
-			# set the url to the browser
-			self.browser.setUrl(q)
+		# 				# set the url to the browser
+		# 				self.browser.setUrl(q)
 
-		return
+		# 		return
 
-	# method for updating url
-	# this method is called by the QWebEngineView object
-	def update_urlbar(self, q):
+		# method for updating url
+		# this method is called by the QWebEngineView object
+		# def update_urlbar(self, q):
 
-		if self.urlbar:
-			# setting text to the url bar
-			self.urlbar.setText(q.toString())
+		# 		if self.urlbar:
+		# 			# setting text to the url bar
+		# 			self.urlbar.setText(q.toString())
 
-			# setting cursor position of the url bar
-			self.urlbar.setCursorPosition(0)
+		# 			# setting cursor position of the url bar
+		# 			self.urlbar.setCursorPosition(0)
 
-		return
+		# 		return
 
-	def closeEvent(self, event):
-		geometry = self.saveGeometry()
-		self.settings.setValue('geometry', geometry)
-		super(MainWindow, self).closeEvent(event)
-		return
-
+		# def closeEvent(self, event):
+		# 		geometry = self.saveGeometry()
+		# 		self.settings.setValue('geometry', geometry)
+		# 		super(MainWindow, self).closeEvent(event)
+		# 		return
 
 
 #  ====== main
