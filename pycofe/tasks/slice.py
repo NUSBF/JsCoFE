8e4d92ebd83422df5223ffbe598beac87c2fa3dd
Maria Fando <b_with_d@outlook.com>
2024-03-28 13:44:39 +0000

    fix(slice'n'dice): xyz_sourse adjustment
    in case the b-factor correction wasn't specified, xyz_sourse won't be
    specified

--- pycofe/tasks/slice.py
+++ pycofe/tasks/slice.py
@@ -67,8 +67,10 @@ class Slice(basic.TaskDriver):
             cmd += ['-xyz_source', 'alphafold']
         elif xyz.BF_correction=="rosetta-suggested":
             cmd += ['-xyz_source', 'rosetta']
-        else:
-            cmd += ['-xyz_source', 'alphafold_bfactor']
+            
+        self.stdoutln("&ˆ&% "+str(xyz.BF_correction))
+        # else:
+        #     cmd += ['-xyz_source', 'alphafold_bfactor']
 
         if int(plddt_threshold)!=0:
             cmd += ["-plddt_threshold",plddt_threshold]
