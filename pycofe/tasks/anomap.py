333c92085ce8ac7959781371b9c1ab9152064bae
krissinel <eugene.krissinel@stfc.ac.uk>
2024-04-21 16:53:24 +0100

    feature complete version 1

--- pycofe/tasks/anomap.py
+++ pycofe/tasks/anomap.py
@@ -3,7 +3,7 @@
 #
 # ============================================================================
 #
-#    20.04.24   <--  Date of Last Modification.
+#    21.04.24   <--  Date of Last Modification.
 #                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ----------------------------------------------------------------------------
 #
@@ -32,7 +32,6 @@ from  pycofe.tasks  import basic
 from  pycofe.dtypes import dtype_template
 
 
-
 # ============================================================================
 # Make AnoMap driver
 
@@ -45,44 +44,11 @@ class AnoMap(basic.TaskDriver):
 
         # fetch input data
 
-        # revision = self.makeClass ( self.input_data.data.revision[0] )
+        revision = self.makeClass ( self.input_data.data.revision[0] )
         hkl      = self.makeClass ( self.input_data.data.hkl     [0] )
         istruct  = self.makeClass ( self.input_data.data.istruct [0] )
 
         # --------------------------------------------------------------------
-
-        # labin_fo = hkl.getMeanF()
-        # if labin_fo[2]!="F":
-        #     self.fail ( "<h3>No amplitude data.</h3>" +\
-        #             "This task requires F/sigF columns in reflection data, " +\
-        #             "which were not found.",
-        #             "No amplitude data." )
-        #     return
-
-        # labin_fo[2] = hkl.getFreeRColumn()
-        # input_mtz   = "_input.mtz"
-
-        # if hasattr(istruct,"FC"):
-        #     labin_fc = [istruct.FC,istruct.PHI]
-        # elif istruct.refiner=="refmac":
-        #     labin_fc = ["FC_ALL_LS",istruct.PHI]
-        # elif istruct.refiner=="buster":
-        #     labin_fc = ["FC",istruct.PHI]
-        # else:
-        #     self.fail ( "<h3>No calculated amplitude data.</h3>" +\
-        #             "This task requires calculated Fc columns in reflection data, " +\
-        #             "which were not found.",
-        #             "No calculated amplitude data." )
-        #     return
-
-        # self.makePhasesMTZ (
-        #         hkl.getHKLFilePath(self.inputDir())    ,labin_fo,
-        #         istruct.getMTZFilePath(self.inputDir()),labin_fc,
-        #         input_mtz )
-        
-        # mapout = self.getMapOFName()
-        # mapout = dtype_template.makeDataId(self.job_id,1) + "_" + self.getMapOFName()
-        # mapout_fpath = os.path.join ( self.outputDir(),mapout )
 
         labin = hkl.getAnomalousFColumns()
         if labin[4]!="F":
@@ -98,11 +64,11 @@ class AnoMap(basic.TaskDriver):
 
         keywords = [
             "MODE EP_SAD",
-            "ROOT \""      + self.outputFName + "\"",
-            "HKLIN "       + hkl.getHKLFilePath(self.inputDir()),
-            "CELL "        + hkl.getCellParameters_str(),
-            "SPACEGROUP "  + hkl.getSpaceGroup(),
-            "WAVELENGTH "  + str(wavelength),
+            "ROOT \""     + self.outputFName + "\"",
+            "HKLIN "      + hkl.getHKLFilePath(self.inputDir()),
+            "CELL "       + hkl.getCellParameters_str(),
+            "SPACEGROUP " + hkl.getSpaceGroup(),
+            "WAVELENGTH " + str(wavelength),
             "CRYSTAL Scatterers DATASET " + hkl.getDataSetName() +\
                       " LABIN  F(+) = "   + labin[0] +\
                             " SIGF(+) = " + labin[1] +\
@@ -131,22 +97,38 @@ class AnoMap(basic.TaskDriver):
 
         have_results = False
 
-        summary = "failed to calculate maps"
+        summary    = "failed to calculate maps"
+        phaser_mtz = None
         anomap_mtz = None
         for fname in flist:
             if fname.endswith("llgmaps.mtz"):
                 anomap_mtz = fname
+            elif fname.endswith(".mtz"):
+                phaser_mtz = fname
 
-        if anomap_mtz:
-            
+        if phaser_mtz and anomap_mtz:
+
+            output_mtz = self.getMTZOFName()
+            self.makeMTZ ( [
+                { "path"   : phaser_mtz,
+                  "labin"  : ["all"],
+                  "labout" : ["all"]
+                },{
+                  "path"   : anomap_mtz,
+                  "labin"  : ["FLLG_AX","PHLLG_AX"],
+                  "labout" : ["DELFWT","PHDELWT"]
+                }
+            ],output_mtz )
+
             struct = self.registerStructure ( 
                             istruct.getMMCIFFilePath(self.inputDir()),
                             istruct.getPDBFilePath(self.inputDir()),
                             None,
-                            anomap_mtz,
-                            libPath = istruct.getLibFilePath(self.inputDir()),
-                            leadKey = istruct.leadKey,
-                            refiner = istruct.refiner 
+                            output_mtz,
+                            libPath    = istruct.getLibFilePath(self.inputDir()),
+                            leadKey    = istruct.leadKey,
+                            # map_labels = "FWT,PHWT,FLLG_AX,PHLLG_AX",
+                            refiner    = istruct.refiner 
                         )
 
             if struct:
@@ -157,65 +139,25 @@ class AnoMap(basic.TaskDriver):
                 struct.makeXYZSubtype   ()
                 struct.copyLabels       ( istruct )
                 struct.copyLigands      ( istruct )
-                #if ligand:
-                #    struct.addLigand ( ligand.code )
-                # struct.setLigands       ( ligList )
-
-                # struct.add_file ( "mol0.mmcif",self.outputDir(),"mmcif",copy_bool=False )
-                # if coot_mmcif:
-                #     struct.add_file ( coot_mmcif,self.outputDir(),"mmcif",copy_bool=False )
-
-                # add link formulas and counts to struct metadata
-                # if link_counts:
-                #     struct.ligands     = link_counts['comps_usr']
-                #     struct.refmacLinks = link_counts['links_usr']
-                #     struct.links       = link_counts['links_std']
-                #     struct.links      += link_counts['links_unk']
+                # struct.FAN  = "FLLG_AX"
+                # struct.PHAN = "PHLLG_AX"
 
                 # create output data widget in the report page
                 self.putTitle ( "Output Structure" )
                 self.putStructureWidget ( "structure_btn","Output Structure",struct )
+                self.putMessage ( "<i style=\"color:darkblue\">" +\
+                    "&nbsp;<p>NOTE: for visualisation purposes, anomalous map " +\
+                    "is presented as difference map in this task's output. To " +\
+                    "use the difference map, branch from task where it was "    +\
+                    "calculated.</i>"
+                )
 
                 # update structure revision
-                # revision.setStructureData ( struct   )
-                # if ligand:
-                #     revision.addLigandData ( ligand      )
-                # if ligand_coot:
-                #     revision.addLigandData ( ligand_coot )
-                # self.registerRevision ( revision )
+                revision.setStructureData ( struct   )
+                self.registerRevision     ( revision )
 
-                # have_results = True
-
-                summary = "anomalous map calculated" 
-
-        # if os.path.isfile(mapout_fpath):
-        #     grid_id = self.getWidgetId ( "grid" )
-        #     self.putGrid ( grid_id )
-        #     self.putMessage1 ( grid_id,"<b>Omit map:</b>&nbsp;",0,0 )
-        #     if format=="CCP4":
-        #         self.putUglyMolButton ( "","",mapout_fpath, # structure.getDMapFilePath(self.outputDir()),
-        #                                 mapout,"Display",grid_id,0,1 )
-        #     self.putDownloadButton ( mapout_fpath,"Export",grid_id,0,2 )
-        #     sfo = float(scale_fobs)
-        #     sfc = float(scale_fc)
-        #     summary = ""
-        #     if sfo!=0.0:
-        #         if sfo==-1.0:
-        #             summary = "-"
-        #         elif sfo!=1.0:
-        #             summary = str(scale_fobs) + "*"
-        #         summary += "F<sub>o</sub>"
-        #     if sfc!=0.0:
-        #         if sfc==-1.0:
-        #             summary += "-"
-        #         elif sfc==1.0:
-        #             summary += "+"
-        #         else:
-        #             if sfc>0.0:
-        #                 summary += "+"
-        #             summary += str(scale_fobs) + "*"
-        #         summary += "F<sub>c</sub>"
-        #     summary += " map calculated"
+                have_results = True
+                summary      = "anomalous map calculated" 
 
         # this will go in the project tree line
         self.generic_parser_summary["anomap"] = {
@@ -223,7 +165,7 @@ class AnoMap(basic.TaskDriver):
         }
 
         # close execution logs and quit; "False" because no data passed on follow-up jobs
-        self.success ( False )
+        self.success ( have_results )
 
         return
 
