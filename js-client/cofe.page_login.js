ee0305c9162bfa92768b6fccdfead447729b8998
krissinel <eugene.krissinel@stfc.ac.uk>
2024-04-27 22:24:41 +0100

    dev

--- js-client/cofe.page_login.js
+++ js-client/cofe.page_login.js
@@ -47,20 +47,20 @@ function LoginPage ( sceneId )  {
 
   // make login panel
   let panel = new Grid('');
-  panel.setWidth ( '300px' );
+  panel.setWidth ( '640px' );
 
-  this.grid.setCellSize            ( '45%','',0,0,1,1 );
+  this.grid.setCellSize            ( '45%','', 0,0,1,1 );
   this.grid.setCellSize            ( '','15px',0,0,1,3 );
-  this.grid.setWidget              ( panel,1,1,1,1 );
+  this.grid.setWidget              ( panel,    1,1,1,1 );
   this.grid.setVerticalAlignment   ( 1,1,'middle' );
-  this.grid.setCellSize            ( '10%','',1,1,1,1 );
-  this.grid.setCellSize            ( '45%','',1,2,1,1 );
+  this.grid.setCellSize            ( '10%','', 1,1,1,1 );
+  this.grid.setCellSize            ( '45%','', 1,2,1,1 );
 
   let tip_lbl = this.grid.setLabel ( '&nbsp;',2,0,1,3 ).setFontSize('80%');
   this.grid.setHorizontalAlignment ( 2,0,'center' );
   this.grid.setVerticalAlignment   ( 2,0,'top'    );
   this.grid.setCellSize            ( '','20px',2,0,1,3 );
-  this.grid.setCellSize            ( '','3px' ,3,0,1,3 );
+  this.grid.setCellSize            ( '','3px', 3,0,1,3 );
 
   this.makeLogoPanel               ( 4,0,3 );
 
@@ -87,11 +87,25 @@ function LoginPage ( sceneId )  {
     },1000);
   }
 
-  let login_lbl   = new Label     ( 'Login name:' );
-  let pwd_lbl     = new Label     ( 'Password:'   );
-  let login_inp   = new InputText ( '' );
-  let pwd_inp     = new InputText ( '' );
-  let vis_btn     = new ImageButton ( image_path('pwd_hidden'),'32px','14px' );
+  let row = 0;
+  let col = 2;
+
+  panel.setLabel ( '<b>Collaborative Computational Project No.4</b><br>' +
+                   '<div style="font-size:80%;padding-bottom:28px;">' +
+                   'Science and Technology Facilities Council UK<br>' +
+                   'Rutherford Appleton Laboratory<br>' +
+                   'Didcot, Oxon, OX1 0FA, United Kingdom<br>' +
+                   '<a href="https://www.ccp4.ac.uk" target="_blank">' +
+                   'https://www.ccp4.ac.uk</a></div>',
+                   row++,col+2,1,3 )
+      //  .setFont  ( 'times','12px',true,true )
+       .setNoWrap();
+
+  let login_lbl = new Label       ( 'Login name:' );
+  let pwd_lbl   = new Label       ( 'Password:'   );
+  let login_inp = new InputText   ( '' );
+  let pwd_inp   = new InputText   ( '' );
+  let vis_btn   = new ImageButton ( image_path('pwd_hidden'),'32px','14px' );
   login_lbl.setNoWrap();
   login_lbl.setFontSize         ( '125%' );
   pwd_lbl  .setFontSize         ( '125%' );
@@ -100,11 +114,6 @@ function LoginPage ( sceneId )  {
                                   'Your CCP4 login','' );
   vis_btn  .setTooltip          ( 'Toggle password visibility' );
   vis_btn  .icon_hidden = true;
-  /*
-                                  'Login name should contain only latin ' +
-                                  'letters, numbers,\n underscores, dashes ' +
-                                  'and dots, and must start with a letter' );
-  */
   login_inp.setFontItalic       ( true   );
   pwd_inp  .setFontSize         ( '112%' );
   pwd_inp  .setStyle            ( 'password','','Your CCP4 password','' );
@@ -112,25 +121,24 @@ function LoginPage ( sceneId )  {
   login_inp.setWidth            ( '95%'  );
   pwd_inp  .setWidth            ( '95%'  );
 
-  let row = 1;
-  panel.setLabel ( appName() + ' Login', row,0,1,3 )
-       .setFont  ( 'times','40px',true,true )
+  panel.setLabel ( appName() + ' Login', row++,col,1,3 )
+       .setFont  ( 'times','32px',true,true )
        .setNoWrap();
-  panel.setHorizontalAlignment  ( row++ ,0,'center' );
-  panel.setWidget               ( this.makeSetupNamePanel(), row++,0,1,3 );
-  panel.setCellSize             ( '','10px',row++,0 );
+  // panel.setHorizontalAlignment  ( row++ ,0,'center' );
+  panel.setWidget               ( this.makeSetupNamePanel(), row++,col,1,3 );
+  panel.setCellSize             ( '','10px',row++,col );
 
-  panel.setWidget               ( login_lbl,row  ,0,1,1 );
-  panel.setWidget               ( pwd_lbl  ,row+1,0,1,1 );
-  panel.setVerticalAlignment    ( row  ,0,'middle' );
-  panel.setVerticalAlignment    ( row+1,0,'middle' );
-  panel.setWidget               ( login_inp,row++,1,1,1 );
-  panel.setWidget               ( pwd_inp  ,row  ,1,1,1 );
-  panel.setWidget               ( vis_btn  ,row  ,2,1,1 );
+  panel.setWidget               ( login_lbl,row  ,col,1,1 );
+  panel.setWidget               ( pwd_lbl  ,row+1,col,1,1 );
+  panel.setVerticalAlignment    ( row  ,col,'middle' );
+  panel.setVerticalAlignment    ( row+1,col,'middle' );
+  panel.setWidget               ( login_inp,row++,col+1,1,1 );
+  panel.setWidget               ( pwd_inp  ,row  ,col+1,1,1 );
+  panel.setWidget               ( vis_btn  ,row  ,col+2,1,1 );
   panel.setVerticalAlignment    ( row++,2,'middle' );
 
-  panel.setCellSize             ( '','4px',row++,0 );
-  panel.setWidget               ( new HLine('3px'), row++,0,1,3 );
+  panel.setCellSize             ( '','4px',row++,col );
+  panel.setWidget               ( new HLine('3px'), row++,col,1,3 );
   //panel.setCellSize             ( '','1px',row++,0 );
 
   let login_btn = new Button    ( 'Login',image_path('login') );
@@ -140,26 +148,24 @@ function LoginPage ( sceneId )  {
   login_btn.setWidth            ( '100%' );
   pwd_btn  .setWidth            ( '100%' );
   reg_btn  .setWidth            ( '100%' );
-  panel.setWidget               ( login_btn,row++,0,1,3 );
-  panel.setWidget               ( pwd_btn  ,row++,0,1,3 );
-  panel.setWidget               ( reg_btn  ,row++,0,1,3 );
-
-  //panel.setLabel                ( '&nbsp;',row++,0,1,3 )
-  panel.setCellSize             ( '','6px',row++,0 );
+  panel.setWidget               ( login_btn,row++,col,1,3 );
+  panel.setWidget               ( pwd_btn  ,row++,col,1,3 );
+  panel.setWidget               ( reg_btn  ,row++,col,1,3 );
+  panel.setCellSize             ( '','6px' ,row++,col );
 
   if (__any_mobile_device)  {
     panel.setLabel              ( '<center><i>Note: Coot and some other tasks ' +
                                   'cannot be used when<br>working ' +
                                   'from tablets and phones<br>&nbsp;' +
                                   '</i></center>',
-                                  row++,0,1,3 )
+                                  row++,col,1,3 )
          .setTooltip            ( 'For best experience, access this web-site from ' +
                                   'MS Windows, Linux or Mac OSX device  with ' +
                                   'CCP4 Software Suite version 7.1 or higher ' +
                                   'installed.'
                                 )
          .setNoWrap();
-    panel.setCellSize           ( '','24px',0,3 );
+    panel.setCellSize           ( '','24px',col,3 );
   } else if (!__local_service)  {
     panel.setLabel              ( '<div style="min-width:380px"><center><i>' +
                                   '<b>NOTE:</b> For using <span style="color:maroon">' +
@@ -170,8 +176,8 @@ function LoginPage ( sceneId )  {
                                   '<img src="images_com/ccp4cloud_remote.png" ' +
                                   'style="height:36px;width:36px;padding-top:6px;"/>' +
                                   '</i></center></div>',
-                                  row++,0,1,3 );
-    panel.setCellSize           ( '','48px',0,3 );
+                                  row++,col,1,3 );
+    panel.setCellSize           ( '','48px',col,3 );
   }
 
   if (!isProtectedConnection())
@@ -179,18 +185,16 @@ function LoginPage ( sceneId )  {
                                   'Connection is not secure – <span style="color:maroon">' +
                                   '<b>Moorhen</b></span> will not work' +
                                   '</i></center></div>',
-                                  row++,0,1,3 );
+                                  row++,col,1,3 );
 
   panel.setLabel                ( '<center><i>Check ' + appName() +
                                   ' <a target="_blank" href="html/roadmap.html">' +
                                   'roadmap<a></i> for new users</center>',
-                                  row++,0,1,3 );
+                                  row++,col,1,3 );
   panel.setLabel                ( '<center style="padding-top:6px;"><i>' +
                                   '<a href="javascript:_privacyStatement()">' +
                                   'Privacy Statement<a></i></center>',
-                                  row++,0,1,3 );
-  // panel.setCellSize             ( '','24px',row++,0 );
-
+                                  row++,col,1,3 );
 
   vis_btn.addOnClickListener ( function(){
     vis_btn.icon_hidden = !vis_btn.icon_hidden;
@@ -273,6 +277,10 @@ function LoginPage ( sceneId )  {
         '</h3>please log in to your ' + appName() + ' account now.',
         'msg_information' );
   }
+
+  panel.setImage ( image_path('ccp4cloud_remote'), '300px','300px', 0,0,row,1 );
+  panel.setLabel ( '&nbsp;',0,1,row,1 ).setWidth('40px');
+  panel.setVerticalAlignment ( 0,0,'middle' );
 
 }
 
