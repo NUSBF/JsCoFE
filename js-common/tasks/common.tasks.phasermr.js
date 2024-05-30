694fb48d7776d334980ff4116924a48038a80801
krissinel <eugene.krissinel@stfc.ac.uk>
2024-05-30 12:33:03 +0100

    phaser/molrep hasLigands fix

--- js-common/tasks/common.tasks.phasermr.js
+++ js-common/tasks/common.tasks.phasermr.js
@@ -2,7 +2,7 @@
 /*
  *  =================================================================
  *
- *    19.05.24   <--  Date of Last Modification.
+ *    30.05.24   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  -----------------------------------------------------------------
  *
@@ -978,7 +978,7 @@ if (!__template)  {
        // this.input_data.data['xmodel'] = [revision.Structure];
 
       if (revision.Structure)  {
-        if (revision.hasLigands())
+        if (revision.subtype.indexOf('ligands')>=0)
           this.input_data.data['ligands'] = [revision.Structure];
         if (revision.Options.mr_type=='sph')
           this.input_data.data['phases'] = [revision.Structure];
