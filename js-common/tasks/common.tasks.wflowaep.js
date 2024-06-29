d8b90a5a6edca66b3a1d59162d0fdee63f8711e3
krissinel <eugene.krissinel@stfc.ac.uk>
2024-06-29 23:21:03 +0100

    feature complete

--- js-common/tasks/common.tasks.wflowaep.js
+++ js-common/tasks/common.tasks.wflowaep.js
@@ -2,7 +2,7 @@
 /*
  *  =================================================================
  *
- *    01.06.24   <--  Date of Last Modification.
+ *    29.06.24   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  -----------------------------------------------------------------
  *
@@ -168,7 +168,7 @@ TaskWFlowAEP.prototype.desc_title     = function()  {
 //TaskWFlowAEP.prototype.platforms = function()  { return 'LMU'; }  // UNIX only
 
 TaskWFlowAEP.prototype.currentVersion = function()  {
-  var version = 1;
+  let version = 1;
   if (__template)
         return  version + __template.TaskTemplate.prototype.currentVersion.call ( this );
   else  return  version + TaskTemplate.prototype.currentVersion.call ( this );
@@ -198,7 +198,7 @@ if (!__template)  {
 } else  {
   // for server side
 
-  var conf = require('../../js-server/server.configuration');
+  const conf = require('../../js-server/server.configuration');
 
   TaskWFlowAEP.prototype.getCommandLine = function ( jobManager,jobDir )  {
     return [conf.pythonName(), '-m', 'pycofe.tasks.wflow_aep', jobManager, jobDir, this.id];
