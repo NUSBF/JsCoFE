edb8e6d1c8c178e9a98819d7a30735665d36e3e2
krissinel <eugene.krissinel@stfc.ac.uk>
2024-10-22 17:32:16 +0100

    version change

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -26,7 +26,7 @@
 function appName()  { return 'CCP4 Cloud'   }  // application name for reporting
 
 // const jsCoFE_version = '1.7.024 [18.07.2024]';   // for the main server
-const jsCoFE_version = '1.8.002 [21.10.2024]';   // for update
+const jsCoFE_version = '1.8.003 [21.10.2024]';   // for update
 
 function appVersion()  {
   return jsCoFE_version;
