544c3338fa523a36ed950c5eec1129284ecb0376
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2024-12-12 17:18:56 +0000

    cosmetics

--- cofe-browser/npm_install.sh
+++ cofe-browser/npm_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server_dir=/Users/eugene/Projects/CCP4Cloud/jsCoFE
+server_dir=/Users/eugene.krissinel/Projects/CCP4Cloud/jsCoFE
 ccp4_dir=/Applications/ccp4-9
 
 source $ccp4_dir/bin/ccp4.setup-sh
