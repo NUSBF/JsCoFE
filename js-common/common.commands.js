b6534cdcee2e59c528b7375040cb7d8f9258a13e
Maria Fando <b_with_d@outlook.com>
2025-01-29 12:40:51 +0000

    date stamp

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -26,7 +26,7 @@
 function appName()  { return 'CCP4 Cloud'   }  // application name for reporting
 
 // const jsCoFE_version = '1.7.024 [18.07.2024]';   // for the main server
-const jsCoFE_version = '1.8.005 [28.01.2025]';   // for update
+const jsCoFE_version = '1.8.005 [29.01.2025]';   // for update
 
 function appVersion()  {
   return jsCoFE_version;
