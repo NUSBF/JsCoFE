3666880b1e56155493a8a7a38dd6ce4f1e8d7eeb
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2025-02-01 21:08:07 +0000

    Table 1 adjustments

--- pycofe/proc/analyse_structure.py
+++ pycofe/proc/analyse_structure.py
@@ -3,7 +3,7 @@
 #
 # ============================================================================
 #
-#    05.01.25   <--  Date of Last Modification.
+#    01.02.25   <--  Date of Last Modification.
 #                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ----------------------------------------------------------------------------
 #
@@ -25,13 +25,13 @@ def analyze_structure ( file_path ):
     
     # Initialize counters and accumulators
     macromolecular_atoms = 0
-    ligand_atoms         = 0
+    ligand_atoms         = {}
     water_atoms          = 0
     hydrogen_atoms       = 0
     n_chains             = 0
     n_models             = 0
     macro_b_factor       = 0.0
-    ligand_b_factor      = 0.0
+    ligand_b_factor      = {}
     water_b_factor       = 0.0
     
     # Iterate over models and chains in the structure
@@ -55,33 +55,48 @@ def analyze_structure ( file_path ):
                             hydrogen_atoms += 1
                 else:
                     # Count ligand atoms
+                    if not residue.name in ligand_atoms:
+                        ligand_atoms   [residue.name] = 0
+                        ligand_b_factor[residue.name] = 0.0
                     for atom in residue:
-                        ligand_atoms    += 1
-                        ligand_b_factor += atom.b_iso
+                        ligand_atoms   [residue.name] += 1
+                        ligand_b_factor[residue.name] += atom.b_iso
                         if atom.element==gemmi.Element('H') or atom.element==gemmi.Element('D'):
                             hydrogen_atoms += 1
 
     # Compute average B-factors
-    avg_b         = 0.0
-    overall_atoms = macromolecular_atoms + ligand_atoms + water_atoms
+    avg_macro_b = macro_b_factor  / macromolecular_atoms  if macromolecular_atoms  else 0
+    avg_water_b = water_b_factor  / water_atoms           if water_atoms           else 0
+
+    avg_b         = macro_b_factor       + water_b_factor
+    overall_atoms = macromolecular_atoms + water_atoms
+    ligand_atoms_list    = []
+    ligand_b_factor_list = []
+    for name in ligand_atoms:
+        if ligand_atoms[name]>0:
+            overall_atoms += ligand_atoms[name]
+            avg_b         += ligand_b_factor[name]
+            ligand_b_factor[name] = ligand_b_factor[name] / ligand_atoms[name]
+            ligand_atoms_list   .append ( "%d "    % ligand_atoms[name]    + name )
+            ligand_b_factor_list.append ( "%0.1f " % ligand_b_factor[name] + name )
+
     if overall_atoms>0:
-      avg_b = (macro_b_factor + ligand_b_factor + water_b_factor ) / overall_atoms
-    avg_macro_b  = macro_b_factor  / macromolecular_atoms  if macromolecular_atoms  else 0
-    avg_ligand_b = ligand_b_factor / ligand_atoms          if ligand_atoms          else 0
-    avg_water_b  = water_b_factor  / water_atoms           if water_atoms           else 0
-    
+        avg_b = avg_b / overall_atoms
+
     # Return the results
     return {
         "overall_atoms"        : overall_atoms,
         "macromolecular_atoms" : macromolecular_atoms,
         "ligand_atoms"         : ligand_atoms,
+        "ligand_atoms_str"     : " / ".join(ligand_atoms_list),
         "water_atoms"          : water_atoms,
         "hydrogen_atoms"       : hydrogen_atoms,
         "n_chains"             : n_chains,
         "n_models"             : n_models,
         "avg_b"                : avg_b,
         "avg_macro_b"          : avg_macro_b,
-        "avg_ligand_b"         : avg_ligand_b,
+        "avg_ligand_b"         : ligand_b_factor,
+        "avg_ligand_b_str"     : " / ".join(ligand_b_factor_list),
         "avg_water_b"          : avg_water_b,
     }
 
