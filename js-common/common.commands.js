df46663760dbb34dea1137a7de5e88a5408d025d
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2025-02-03 15:53:04 +0000

    morda slurm

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -1,7 +1,7 @@
 /*
  *  ===========================================================================
  *
- *    02.02.25   <--  Date of Last Modification.
+ *    03.02.25   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -26,7 +26,7 @@
 function appName()  { return 'CCP4 Cloud'   }  // application name for reporting
 
 // const jsCoFE_version = '1.7.024 [18.07.2024]';   // for the main server
-const jsCoFE_version = '1.8.005 [02.02.2025]';   // for update
+const jsCoFE_version = '1.8.005 [03.02.2025]';   // for update
 
 function appVersion()  {
   return jsCoFE_version;
