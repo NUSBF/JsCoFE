b60593d5c0e6b0fca91bcb9a2d843e5e1628373e
Earl Talavera <talaveraearl@yahoo.com>
2025-02-07 09:31:47 +0000

    Created input widgets and import files for task, currently displays file name to the user in the output section

--- js-common/tasks/common.tasks.importserial.js
+++ js-common/tasks/common.tasks.importserial.js
@@ -47,31 +47,41 @@ function TaskImportSerial()  {
   this.title       = 'Import Serial';
 
   this.file_select = [{
-      file_types  : '.mtz,.hkl', // data type(s) and subtype(s)
-      label       : 'File #1', // label for input dialog
-      tooltip     : '[Mandatory] Path to MTZ file with merged or unmerged ' +
-                    'reflections.',
-      inputId     : 'hklin1',  // input Id for referencing input fields
+      file_types  : '.hkl', // data type(s) and subtype(s)
+      label       : 'Merged HKL file', // label for input dialog
+      tooltip     : '[Mandatory] Path to HKL Merged data file ', //tooltip is hover over the label in the UI
+      inputId     : 'hklin',  // input Id for referencing input fields
       path        : '',
-      min         : 0           // minimum acceptable number of data instances
+      min         : 1,          // minimum acceptable number of data instances
+      max         : 1
     },{
-      file_types  : '.mtz,.hkl', // data type(s) and subtype(s)
-      label       : 'File #2', // label for input dialog
-      tooltip     : '[Desirable] Path to sequence file in .fasta or .pir ' +
-                    'format. For importing several sequences put them all in a ' +
-                    'single file.',
+      file_types  : '.hkl1,.hkl2', // data type(s) and subtype(s)
+      label       : 'Half data set .HKL1 file', // label for input dialog
+      tooltip     : '[Optional] Path to .HKL1 Half-Merged data set',
       inputId     : 'hklin2',   // input Id for referencing input fields
       path        : '',
-      min         : 0         // minimum acceptable number of data instances
-    },{
-      file_types  : '.mtz', // data type(s) and subtype(s)
-      label       : 'File #3', // label for input dialog
-      tooltip     : '[Optional] Path to CIF file with description of ligand ' +
-                    'to be fitted in electron density blobs',
-      inputId     : 'hklin3', // input Id for referencing input fields
+      min         : 0   ,      // minimum acceptable number of data instances
+      max         : 2
+    },
+    ,{
+      file_types: '.cell',
+      label     : 'Cell file',
+      tooltip   : '[Mandatory] Path to .CELL file ',
+      inputId     : 'cellfile', // input Id for referencing input fields
       path        : '',
-      min         : 0         // minimum acceptable number of data instances
+      min         : 1,   //Minimum files required  
+      max         : 1
+    },{
+      file_types: '.pdb,.mmcif,.mtz',
+      label     : 'Reference file',
+      tooltip   : '[Optional]Path to PDB or mmCIF file or MTZ file to import  spacegroup and unit cell parameters',
+      inputId     : 'referencefile', // input Id for referencing input fields
+      path        : '',
+      min         : 0,   //Minimum files required 
+      showon      : {SOURCE_SEL:['R']}, //Check if this call is available ***
+      max         : 1
     }
+    
   ];
 
   this.input_ligands = [];
@@ -102,6 +112,121 @@ function TaskImportSerial()  {
   //                iwidth   : 140,
   //                position : [1,0,1,3]
   //              }
+
+    SPACER : {  //Empty spacer spacer does
+      type     : 'label',
+      keyword  : 'none',
+      label    : '&nbsp',
+      position : [0,0,1,5]
+    },
+
+    sec1 :  { type     : 'section', //creates segmented sections inside the user interface
+              title    : 'Space Group, Unit Cell Parameters',  //name this for section title
+              open     : false,  // true for the section to be initially open
+              position : [1,0,1,8],
+              contains : {
+
+                SOURCE_SEL : {
+                  type     : 'combobox',
+                  label    : '<b><i>Data Input Preference&nbsp;</i></b>', //Name of title beside the combobox &nbsp = extra space
+                  tooltip  : 'Source for the Space Group and Unit Cell Parameters',
+                  range    : ['R|Reference File',
+                              'SU|Space Group and Unit Cell Parameters',                   //This is the values available inside the dropdown list
+          
+                            ],
+                  value    : 'R',
+                  position : [2,2,1,1]
+                },
+                REFERENCEFILE : {
+                  // keyword   : 'smiles',
+                  // label     : '<i> Reference File </i>',
+                  // tooltip   : '(Optional) Reference File  ',
+                  value     : '',
+                  position  : [3,2,1,1],
+                  iwidth    : 200,            
+                  type: 'file',
+                  label     : 'Reference file',
+                  tooltip   : '[Optional]Path to PDB or mmCIF file or MTZ file to import  spacegroup and unit cell parameters',
+                  inputId     : 'referencefile', // input Id for referencing input fields
+                  path        : '',
+                  min         : 0,   //Minimum files required 
+                  showon      : {SOURCE_SEL:['R']}, //Check if this call is available ***
+                  max         : 1
+                },
+                
+                SPACEGROUP : {
+                  type      : 'string',   // empty string not allowed
+                  label     : '<b><i> Space Group </b></i>',
+                  tooltip   : ' (Mandatory) Required Specification of Space Group ',
+                  value     : '',
+                  position  : [3,2,1,1],
+                  showon    : {SOURCE_SEL:['SU']},
+                  maxlength : 5,       // largest identified protein space group
+                  
+                },
+                UNITCELLPARAMETERS : {
+                  type      : 'string',   // empty string not allowed
+                  label     : '<b><i> Unit Cell Parameters </b></i>',
+                  tooltip   : '(Mandatory) Required Specification of Unit Cell Parameters ',
+                  align     : "left",
+                  value     : '',
+                  position  : [4,2,2,5],
+                  showon    : {SOURCE_SEL:['SU']}
+                  // maxlength : 5  //determine if their is a restraint already
+                },
+            },
+            
+          },
+
+
+    sec2 :  { type     : 'section',
+    title    : 'Wavelength, Dmin, Dmax',
+    open     : false,  // true for the section to be initially open
+    position : [2,0,1,8],
+    contains : {
+      WAVELENGTH : {
+        type      : 'real',   // empty string not allowed
+        label     : '<i> <b> Wavelength </b> </i>',
+        tooltip   : ' (Mandatory) Wavelength (only for data from CrystFEL) ',
+        align     : "left",
+        value     : '',
+        iwidth    : 100,
+        range     : [0.00,2.00], //check the range of the high resolution cutoff
+        position  : [0,1,1,7], // grid-row, grid-column ,
+       
+      },
+      DMIN : {
+        type      : 'real_',   // empty string allowed
+        label     : '<i> <b> High-resolution cutoff </b> </i>',
+        tooltip   : '(Optional) High-resolution cutoff for data',
+        default   : '',
+        value     : '',
+        iwidth    : 100,
+        range     : [0.00,2.00], //check the range of the high resolution cutoff
+        maxlength : 3,       // maximum input length
+        lwidth2   : '100%',
+        position  : [2,1,1,7],
+      
+          },
+      DMAX : {
+        type      : 'real_',   // empty string allowed
+        label     : '<i> <b> Low-resolution cutoff </b> </i>',
+        tooltip   : '(Optional) Low-resolution cutoff for data',
+        default   : '',
+        value     : '',
+        iwidth    : 100,
+        range     : [0.00,2.00], //check the range of the high resolution cutoff
+        maxlength : 3,       // maximum input length
+        lwidth2   : '100%',
+        position  : [3,1,1,7],
+      },
+     
+    }
+  }
+
+    
+
+
   };
 
 }
@@ -116,9 +241,15 @@ TaskImportSerial.prototype.icon           = function()  { return 'task_wflowafmr
 TaskImportSerial.prototype.clipboard_name = function()  { return '"Import Serial"'; }
 
 TaskImportSerial.prototype.desc_title     = function()  {  // *EK* edit description
-  return 'data import, ASU definition, automatic MR, refinement, ligand fitting and PDB validation report';
+  return 'upload and import .hkl, .mtz, .cell files from your device to merge and convert to MTZ Format ';
 }
 
+TaskImportSerial.prototype.taskDescription = function()  {
+  // this appears under task title in the Task Dialog
+    return 'Upload and import .hkl, .mtz, .cell files from your device to merge and convert to MTZ Format ';
+  }
+
+    
 
 // *EK* Can ImportSerial work on Windows?
 
