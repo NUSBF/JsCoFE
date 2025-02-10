76441c6c446a637df560044b2795510e41faa12f
Earl Talavera <talaveraearl@yahoo.com>
2025-02-10 17:02:00 +0000

    Added keywords and create cmd command line, retrieving the user input and files imported.

--- pycofe/tasks/importserial.py
+++ pycofe/tasks/importserial.py
@@ -44,7 +44,8 @@ class ImportSerial(import_task.Import):
 
     def run(self): #This is how the apps are ran
 
-        self.putMessage ( "<b>This is FIRST TRY. HELLO!!!</b>" ) #Display message to the user in the output
+        self.putMessage("<b><h3> File Existance Check </h3></b> ") #Heading , remove later
+        self.putMessage(" ") #Spacing , remove later
 
         hklin = os.path.join ( self.importDir(),self.task.file_select[0].path ) # Creates the import button functionality
         halfdataset1 = os.path.join ( self.importDir(),self.task.file_select[1].path )  #can't have a - in the name
@@ -58,8 +59,12 @@ class ImportSerial(import_task.Import):
         self.putMessage ( "\n" + str(cellfile) + " : " + (" exists" if os.path.isfile(cellfile) else " does not exist") )
         self.putMessage ( "\n" + str(reference) + " : " + (" exists" if os.path.isfile(reference) else " does not exist") )
 
-        # Prepare import serial input
-        # fetch input data
+        # --------------------------- cmd option keywords-------------------------------
+
+        cmd = [
+            "-m"         , "import_serial",
+            "--hklin"    , hklin,
+        ]
 
         
         # --------------------------- Retrieving input data-------------------------------
@@ -72,7 +77,7 @@ class ImportSerial(import_task.Import):
         # half-dataset2       = self.makeClass ( self.input_data.data. half-dataset1    [0] )
         # reference           = self.makeClass ( self.input_data.data.reference         [0] )
         # cellfile            = self.makeClass ( self.input_data.data.cellfile          [0] )
-        # cell            = self.makeClass ( self.input_data.data.cell                  [0] )
+        # cell                = self.makeClass ( self.input_data.data.cell              [0] )
         # wavelength          = self.makeClass ( self.input_data.data.wavelength        [0] )
         # spacegroup          = self.makeClass ( self.input_data.data.spacegroup        [0] )
         # dmin                = self.makeClass ( self.input_data.data.dmin              [0] ) 
@@ -82,54 +87,81 @@ class ImportSerial(import_task.Import):
         sec1 = self.task.parameters.sec1.contains #Call section 1 containing all the parameters
         sec2 = self.task.parameters.sec2.contains #Call section 2
 
+        self.putMessage(" ") #Spacing , remove later
+        self.putMessage("<b><h3> Cmd Parameter Keywords and Value </h3></b> ") #Heading , remove later
+        self.putMessage(" ") #Spacing , remove later
+
         wavelength = self.getParameter ( sec1.WAVELENGTH ).strip()
         if wavelength: # If there is wavelength user input
             wavelength = str(wavelength)
+            cmd += [ "--wavelength", str(wavelength) ]
+            self.putMessage(str(cmd)) 
 
         spacegroup = self.getParameter ( sec1.SPACEGROUP ).strip()
         if spacegroup: # If there is spacegroup user input
             spacegroup = str(spacegroup)
+            cmd += [ "--spacegroup", str(spacegroup) ]
+            self.putMessage(str(cmd))  
 
         cell = self.getParameter ( sec1.UNITCELLPARAMETERS ).strip()
         if cell: # If there is cell user input
             cell = str(cell)
+            cmd += [ "--cell", str(cell) ]
+            self.putMessage(str(cmd)) 
 
         dmin = self.getParameter ( sec2.DMIN ).strip()
         if cell: # If there is High-resolution cutoff user input
             cell = str(dmin)
+            cmd += [ "--dmin", str(dmin) ]
+            self.putMessage(str(cmd)) 
 
         dmax = self.getParameter ( sec2.DMAX ).strip()
         if cell: # If there is Low-resolution cutoff user input
             cell = str(dmax)
+            cmd += [ "--dmax", str(dmax) ]
+            self.putMessage(str(cmd)) 
+
+         #retrieve the keywords from file input 
+
+        #If .hkl1 file was provided
+        if os.path.isfile(halfdataset1) != '':
+            cmd += [ "--hkl1", str(halfdataset1) ]
+            self.putMessage(str(cmd)) 
+
+        #If .hkl2 file was provided
+        if os.path.isfile(halfdataset2) != '':
+            cmd += [ "--hkl2",str(halfdataset2) ]
+            self.putMessage(str(cmd))        
+
+        #If cell file was provided
+        if os.path.isfile(cellfile) != '':
+            cmd += [ "--cellfile", str(cellfile) ]
+            self.putMessage(str(cmd))
+
+        #If reference file was provided
+        if os.path.isfile(reference) != '':
+            cmd += [ "--reference", str(reference) ]
+            self.putMessage(str(cmd))
 
 
+
+        self.putMessage("<b>Import Serial cmd command :</b>" + " ".join(cmd))
+
+        self.putMessage(" ") #Spacing , remove later
         
       # ------------------------------- Display the value of the data to the output -----------------------
 
+        self.putMessage("<b><h3> Parameters Input Value </h3></b> ") #Heading , remove later
+        self.putMessage(" ") #Spacing , remove later
 
-        self.putMessage( "\n" + " Wavelength is "+ str(wavelength) )
-        self.putMessage( "\n" + " Spacegroup is "+ str(spacegroup) )
-        self.putMessage( "\n" + " Unit cell parameters are "+ str(cell) )
-        self.putMessage( "\n" + " High Resolution Cutoff is "+ str(dmin) )
-        self.putMessage( "\n" + " Low Resolution Cutoff is "+ str(dmax) )
+        self.putMessage( "\n" + " <b>Wavelength is </b>"+ str(wavelength) )
+        self.putMessage( "\n" + " <b>Spacegroup is </b>"+ str(spacegroup) )
+        self.putMessage( "\n" + " <b>Unit cell parameters are </b>"+ str(cell) )
+        self.putMessage( "\n" + " <b>High Resolution Cutoff is </b>"+ str(dmin) )
+        self.putMessage( "\n" + " <b>Low Resolution Cutoff is </b>"+ str(dmax) )
         
-        
-         
 
-        # --------------------------- cmd option keywords-------------------------------
-
-        # cmd = [
-        #     "-m"         , "pairef",
-        #     "--ccp4cloud",
-        #     "--project"  , self.pairefProject(),
-        #     "--XYZIN"    , xyzin,
-        #     "--HKLIN"    , hklin,
-        #     "-r"         , ",".join(resList[1:]),
-        #     "-i"         , str(resList[0])
-        # ]
-
-
-
+       
 
 
 
