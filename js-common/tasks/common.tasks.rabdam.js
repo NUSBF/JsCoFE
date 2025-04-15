9ec67bfb6a2c91ac9559604ac581cd9e2b09d421
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2025-04-15 22:40:23 +0100

    checking on data suitability

--- js-common/tasks/common.tasks.rabdam.js
+++ js-common/tasks/common.tasks.rabdam.js
@@ -1,7 +1,7 @@
 /*
  *  =================================================================
  *
- *    07.04.25   <--  Date of Last Modification.
+ *    15.04.25   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  -----------------------------------------------------------------
  *
@@ -64,21 +64,17 @@ TaskRabdam.prototype.icon           = function()  { return 'task_rabdam'; }
 TaskRabdam.prototype.clipboard_name = function()  { return '"Rabdam"';    }
 
 TaskRabdam.prototype.checkKeywords = function ( keywords )  {
-  // keywords supposed to be in low register
-    return this.__check_keywords ( keywords,['rabdam','radiation','damage','analysis'] );
-  }
-
-
+// keywords supposed to be in low register
+  return this.__check_keywords ( keywords,['rabdam','radiation','damage','analysis'] );
+}
 
 TaskRabdam.prototype.currentVersion = function()  {
-  var version = 1;
+  let version = 1;
   if (__template)
         return  version + __template.TaskTemplate.prototype.currentVersion.call ( this );
         
   else  return  version + TaskTemplate.prototype.currentVersion.call ( this );
 }
-
-
 
 // export such that it could be used in both node and a browser
 
@@ -90,15 +86,38 @@ if (!__template)  {
     return 'estimates the extent of specific radiation damage';
   }
 
-  
+
+  TaskRabdam.prototype.collectInput = function ( inputPanel )  {
+
+    let input_msg = TaskTemplate.prototype.collectInput.call ( this,inputPanel );
+
+    let ixyz  = this.input_data.getData ( 'ixyz' )[0];
+    let ixyz0 = ixyz._type=='DataRevision' ? ixyz.Structure : ixyz;
+
+    let msg = [];
+    if ((ixyz0.BF_correction!='none') && (ixyz0.BF_correction!='pdb'))
+      msg.push ( 'likely a predicted model (no radiation damage)' );
+    if (ixyz0.xyzmeta.xyz.length>1)
+      msg.push ( 'several models found - likely an NMR structure (no radiation damage)' );
+    
+    if (msg.length>0)
+      input_msg = '#Unsuitable input data#<h2>Unsuitable input data</h2>' +
+                  'Selected data is unsuitable for Rabdam analysis because:' +
+                  '<ul><li><b>' + msg.join('</b></li><li><b>') +
+                  '</b></li></ul>Please select the model derived from the X-ray ' +
+                  'diffraction data.#msg_error';
+
+    return input_msg;
+
+  }
 
 } else  {
   //  for server side
 
-  var conf = require('../../js-server/server.configuration');
+  const conf = require('../../js-server/server.configuration');
 
   TaskRabdam.prototype.makeInputData = function ( loginData,jobDir )  {
-    var ixyz = this.input_data.data['ixyz'][0];
+    let ixyz  = this.input_data.data['ixyz'][0];
     if (ixyz._type=='DataRevision')
       this.input_data.data['istruct'] = [ixyz.Structure];
     __template.TaskTemplate.prototype.makeInputData.call ( this,loginData,jobDir );
