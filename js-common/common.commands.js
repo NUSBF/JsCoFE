f58b5f9550d91929396dcdbcfe6f241e1497de38
Eugene Krissinel <eugene.krissinel@stfc.ac.uk>
2025-05-05 22:15:58 +0100

    job dialog detachment and documentation (new tips)

--- js-common/common.commands.js
+++ js-common/common.commands.js
@@ -1,7 +1,7 @@
 /*
  *  ===========================================================================
  *
- *    27.04.25   <--  Date of Last Modification.
+ *    04.05.25   <--  Date of Last Modification.
  *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  --------------------------------------------------------------------------
  *
@@ -26,7 +26,7 @@
 function appName()  { return 'CCP4 Cloud'; }   // application name for reporting
 
 // const jsCoFE_version = '1.7.024 [18.07.2024]';   // for the main server
-const jsCoFE_version = '1.8.007 [01.05.2025]';   // for update
+const jsCoFE_version = '1.8.007 [04.05.2025]';   // for update
 
 function appVersion()  {
   return jsCoFE_version;
